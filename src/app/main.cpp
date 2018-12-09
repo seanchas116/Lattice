400d8e3025b6a1d0bc38a750f9e39981cad7d4dd
Ryohei Ikegami <iofg2100@gmail.com>
2018-12-09 16:16:02 +0900

    Hello

---
+++ src/app/main.cpp
@@ -0,0 +1,24 @@
+#include <QApplication>
+#include <QSurfaceFormat>
+#include <QOpenGLContext>
+#include <QWidget>
+
+int main(int argc, char *argv[]) {
+    QApplication::setAttribute(Qt::AA_UseDesktopOpenGL);
+    QApplication::setAttribute(Qt::AA_UseHighDpiPixmaps);
+
+    QApplication a(argc, argv);
+
+    QSurfaceFormat glFormat;
+    glFormat.setVersion(3, 3);
+    glFormat.setProfile(QSurfaceFormat::CoreProfile);
+    glFormat.setDepthBufferSize(24);
+    glFormat.setSamples(4);
+    glFormat.setOption(QSurfaceFormat::DebugContext); // TODO: unset in release
+
+    QSurfaceFormat::setDefaultFormat(glFormat);
+
+    QWidget window;
+    window.show();
+    return a.exec();
+}
