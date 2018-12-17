fe5b4075d599fb4e9503f573f9a4545c8b9afaf1
Ryohei Ikegami <iofg2100@gmail.com>
2018-12-17 12:03:02 +0900

    Render GridFloor

--- src/viewport/ViewportRenderer.cpp
+++ src/viewport/ViewportRenderer.cpp
@@ -1,21 +1,36 @@
 #include "ViewportRenderer.hpp"
+#include "GridFloor.hpp"
 #include "../gl/Shader.hpp"
 #include "../resource/Resource.hpp"
+#include <glm/gtc/matrix_transform.hpp>
+
+using namespace glm;
 
 namespace Lattice {
 
 ViewportRenderer::ViewportRenderer() {
     initializeOpenGLFunctions();
     _lineShader = std::make_shared<Shader>(readResource("src/viewport/ThickLine.vert"), readResource("src/viewport/ThickLine.geom"), readResource("src/viewport/ThickLine.frag"));
+    _gridFloor = std::make_shared<GridFloor>();
 }
 
 void ViewportRenderer::resize(glm::ivec2 size) {
     glViewport(0, 0, size.x, size.y);
+    _size = size;
+    _projection = glm::perspective(glm::radians(60.f), float(size.x) / float(size.y), 0.1f, 100.f);
 }
 
 void ViewportRenderer::render() {
     glClearColor(0.8f, 0.8f, 0.8f, 1);
     glClear(GL_COLOR_BUFFER_BIT);
+
+    _lineShader->bind();
+    _lineShader->setUniform("zOffset", 0.f);
+    _lineShader->setUniform("MVP", _projection * _camera.matrix());
+    _lineShader->setUniform("width", 1.f);
+    _lineShader->setUniform("color", vec3(0, 0, 0));
+    _lineShader->setUniform("viewportSize", vec2(_size));
+    _gridFloor->draw();
 }
 
 } // namespace Lattice
--- src/viewport/ViewportRenderer.hpp
+++ src/viewport/ViewportRenderer.hpp
@@ -3,11 +3,13 @@
 #include <QObject>
 #include <QOpenGLExtraFunctions>
 #include <glm/glm.hpp>
+#include "../support/Camera.hpp"
 #include "../support/Pointer.hpp"
 
 namespace Lattice {
 
 class Shader;
+class GridFloor;
 
 class ViewportRenderer final : public QObject, protected QOpenGLExtraFunctions {
     Q_OBJECT
@@ -18,7 +20,11 @@ public:
     void render();
 
 private:
+    glm::ivec2 _size;
+    Camera _camera;
+    glm::mat4 _projection;
     SP<Shader> _lineShader;
+    SP<GridFloor> _gridFloor;
 };
 
 } // namespace Lattice
