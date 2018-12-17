2ee373ee3414197b75912114e7bab0a3a086ffa2
Ryohei Ikegami <iofg2100@gmail.com>
2018-12-17 12:02:57 +0900

    Add debug logger

--- src/viewport/ViewportWidget.cpp
+++ src/viewport/ViewportWidget.cpp
@@ -1,5 +1,6 @@
 #include "ViewportWidget.hpp"
 #include "ViewportRenderer.hpp"
+#include <QOpenGLDebugLogger>
 
 namespace Lattice {
 
@@ -8,6 +9,15 @@ ViewportWidget::ViewportWidget(QWidget *parent) : QOpenGLWidget(parent)
 }
 
 void ViewportWidget::initializeGL() {
+    auto logger = new QOpenGLDebugLogger(this);
+    if (logger->initialize()) {
+        connect(logger, &QOpenGLDebugLogger::messageLogged, [](const auto& message) {
+            qDebug() << message;
+        });
+        logger->startLogging();
+        qDebug() << "OpenGL debug enabled";
+    }
+
     _renderer = std::make_shared<ViewportRenderer>();
 }
 
