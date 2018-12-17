318882eab7607d6e67c5a202ada1297cb6114da2
Ryohei Ikegami <iofg2100@gmail.com>
2018-12-18 00:46:09 +0900

    WIP

--- src/viewport/MeshRenderer.cpp
+++ src/viewport/MeshRenderer.cpp
@@ -1,14 +1,34 @@
 #include "MeshRenderer.hpp"
+#include "../document/MeshShape.hpp"
 #include "../gl/VAO.hpp"
 #include "../gl/LineVAO.hpp"
 #include "../gl/VertexBuffer.hpp"
 
+using namespace glm;
+
 namespace Lattice {
 
 MeshRenderer::MeshRenderer() {
-    auto vbo = std::make_shared<VertexBuffer>();
-    _faceVAO = std::make_shared<VAO>(vbo);
-    _edgeVAO = std::make_shared<LineVAO>(vbo);
+    _faceVAO = std::make_shared<VAO>();
+    _edgeVAO = std::make_shared<LineVAO>();
+}
+
+void MeshRenderer::update(const SP<MeshShape> &shape) {
+    {
+        // Edge VAO
+        std::unordered_map<SP<Vertex>, int> indices;
+        std::vector<VertexBuffer::Vertex> vertices;
+        for (auto& vertex : shape->vertices()) {
+            indices[vertex] = vertices.size();
+            VertexBuffer::Vertex vertexData = {
+                    vertex->position(),
+                    vec2(0), // TODO
+                    vec3(0) // TODO
+            };
+            vertices.push_back(vertexData);
+        }
+        // WIP
+    }
 }
 
 }
--- src/viewport/MeshRenderer.hpp
+++ src/viewport/MeshRenderer.hpp
@@ -6,10 +6,13 @@ namespace Lattice {
 
 class VAO;
 class LineVAO;
+class MeshShape;
 
 class MeshRenderer {
 public:
     MeshRenderer();
+    void update(const SP<MeshShape>& shape);
+
 private:
     SP<VAO> _faceVAO;
     SP<LineVAO> _edgeVAO;
