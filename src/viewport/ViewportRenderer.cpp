b507d312881f7cbd39e0f32b12af29e33acf30a5
Ryohei Ikegami <iofg2100@gmail.com>
2018-12-19 16:37:06 +0900

    Create MeshRenderer

--- src/viewport/ViewportRenderer.cpp
+++ src/viewport/ViewportRenderer.cpp
@@ -1,7 +1,13 @@
 #include "ViewportRenderer.hpp"
 #include "GridFloor.hpp"
+#include "MeshRenderer.hpp"
 #include "../gl/Shader.hpp"
 #include "../resource/Resource.hpp"
+#include "../app/AppState.hpp"
+#include "../document/Document.hpp"
+#include "../document/Item.hpp"
+#include "../document/MeshItem.hpp"
+#include "../document/MeshShape.hpp"
 #include <glm/gtc/matrix_transform.hpp>
 
 using namespace glm;
@@ -26,6 +32,14 @@ void ViewportRenderer::resize(glm::ivec2 size) {
 void ViewportRenderer::render() {
     glClearColor(0.8f, 0.8f, 0.8f, 1);
     glClear(GL_COLOR_BUFFER_BIT);
+
+    _appState->document()->rootItem()->forEachDescendant([&] (auto& item) {
+        auto meshItem = std::dynamic_pointer_cast<MeshItem>(item);
+        if (!meshItem) { return; }
+        auto renderer = std::make_shared<MeshRenderer>();
+        renderer->update(meshItem->shape());
+        _meshRenderers[meshItem] = renderer;
+    });
 
     _lineShader->bind();
     _lineShader->setUniform("zOffset", 0.f);
