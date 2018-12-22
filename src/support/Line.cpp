97cddb9b1d8307102272bf794ab748e2abba3b43
Ryohei Ikegami <iofg2100@gmail.com>
2018-12-22 15:08:36 +0900

    Fix distance

--- src/support/Line.cpp
+++ src/support/Line.cpp
@@ -3,9 +3,10 @@
 namespace Lattice {
 
 std::tuple<float, float> Line::distance(glm::vec3 p) const {
-    float t = glm::dot(p - a, direction);
-    glm::vec3 q = a + t * direction;
-    return std::tuple(glm::distance(p, q), t);
+    glm::vec3 w = p - a;
+    float d = length(cross(direction, w));
+    float t = dot(direction, w);
+    return std::tuple(d, t);
 }
 
 } // namespace Lattice
