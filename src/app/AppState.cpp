e0a8a423ef71a1c39941b8a42adb5c74f48c810a
Ryohei Ikegami <iofg2100@gmail.com>
2018-12-22 13:47:58 +0900

    Add AppState currentItem

--- src/app/AppState.cpp
+++ src/app/AppState.cpp
@@ -1,11 +1,25 @@
 #include "AppState.hpp"
 #include "../document/Document.hpp"
+#include "../document/MeshItem.hpp"
 
 namespace Lattice {
 
 AppState::AppState() {
     _document = std::make_shared<Document>();
     _document->addInitialItems();
+    setCurrentItem(_document->rootItem()->childItems()[0]);
+}
+
+SP<MeshItem> AppState::currentMeshItem() const {
+    return std::dynamic_pointer_cast<MeshItem>(_currentItem);
+}
+
+void AppState::setCurrentItem(const SP<Item> &item) {
+    if (item != _currentItem) {
+        _currentItem = item;
+        emit currentItemChanged(item);
+        emit currentMeshItemChanged(currentMeshItem());
+    }
 }
 
 } // namespace Lattice
