4a3f28efe578f1a2d6f3d3cd0a4cafba174e0d34
Ryohei Ikegami <iofg2100@gmail.com>
2018-12-24 02:15:02 +0900

    Rename

--- src/document/Mesh.cpp
+++ src/document/Mesh.cpp
@@ -5,22 +5,22 @@ namespace Lattice {
 Mesh::Mesh() {
 }
 
-SP<Vertex> Mesh::addVertex() {
-    auto vertex = std::make_shared<Vertex>();
+SP<MeshVertex> Mesh::addVertex() {
+    auto vertex = std::make_shared<MeshVertex>();
     _vertices.insert(vertex);
     return vertex;
 }
 
-SP<Edge> Mesh::addEdge(const std::pair<SP<Vertex>, SP<Vertex> > &vertices) {
-    auto edge = std::make_shared<Edge>(vertices, shared_from_this());
+SP<MeshEdge> Mesh::addEdge(const std::pair<SP<MeshVertex>, SP<MeshVertex> > &vertices) {
+    auto edge = std::make_shared<MeshEdge>(vertices, shared_from_this());
     _edges[vertices] = edge;
     _edgesForVertex.insert({vertices.first, edge});
     _edgesForVertex.insert({vertices.second, edge});
     return edge;
 }
 
-SP<Face> Mesh::addFace(const std::vector<SP<Vertex> > &vertices) {
-    auto face = std::make_shared<Face>(vertices, shared_from_this());
+SP<MeshFace> Mesh::addFace(const std::vector<SP<MeshVertex> > &vertices) {
+    auto face = std::make_shared<MeshFace>(vertices, shared_from_this());
     _faces[vertices] = face;
     for (auto& v : vertices) {
         _facesForVertex.insert({v, face});
@@ -42,19 +42,19 @@ std::vector<V> values(const std::unordered_multimap<K, V>& multimap, const K& ke
 
 }
 
-std::vector<SP<Edge> > Mesh::edgesForVertex(const SP<Vertex> &vertex) const {
+std::vector<SP<MeshEdge> > Mesh::edgesForVertex(const SP<MeshVertex> &vertex) const {
     return values(_edgesForVertex, vertex);
 }
 
-std::vector<SP<Face> > Mesh::facesForVertex(const SP<Vertex> &vertex) const {
+std::vector<SP<MeshFace> > Mesh::facesForVertex(const SP<MeshVertex> &vertex) const {
     return values(_facesForVertex, vertex);
 }
 
-std::vector<SP<Edge> > Mesh::edgesForFace(const SP<Face> &face) const {
+std::vector<SP<MeshEdge> > Mesh::edgesForFace(const SP<MeshFace> &face) const {
     return values(_edgesForFace, face);
 }
 
-std::vector<SP<Face> > Mesh::facesForEdge(const SP<Edge> &edge) const {
+std::vector<SP<MeshFace> > Mesh::facesForEdge(const SP<MeshEdge> &edge) const {
     return values(_facesForEdge, edge);
 }
 
--- src/document/Mesh.hpp
+++ src/document/Mesh.hpp
@@ -8,47 +8,47 @@
 
 namespace Lattice {
 
-class Vertex;
-class Edge;
-class Face;
+class MeshVertex;
+class MeshEdge;
+class MeshFace;
 class Mesh;
 
-class Vertex {
+class MeshVertex {
 public:
     SP<Mesh> mesh() const;
     glm::vec3 position() const;
     void setPosition(glm::vec3 position);
 
-    std::vector<SP<Edge>> edges() const;
-    std::vector<SP<Face>> faces() const;
+    std::vector<SP<MeshEdge>> edges() const;
+    std::vector<SP<MeshFace>> faces() const;
 
 private:
     WP<Mesh> _mesh;
 };
 
-class Edge {
+class MeshEdge {
 public:
-    Edge(const std::pair<SP<Vertex>, SP<Vertex>>& vertices, const SP<Mesh>& mesh) : _vertices(vertices), _mesh(mesh) {}
+    MeshEdge(const std::pair<SP<MeshVertex>, SP<MeshVertex>>& vertices, const SP<Mesh>& mesh) : _vertices(vertices), _mesh(mesh) {}
 
     auto mesh() const { return _mesh.lock(); }
     auto& vertices() const { return _vertices; }
-    std::vector<SP<Face>> faces() const;
+    std::vector<SP<MeshFace>> faces() const;
 
 private:
-    std::pair<SP<Vertex>, SP<Vertex>> _vertices;
+    std::pair<SP<MeshVertex>, SP<MeshVertex>> _vertices;
     WP<Mesh> _mesh;
 };
 
-class Face {
+class MeshFace {
 public:
-    Face(const std::vector<SP<Vertex>>& vertices, const SP<Mesh>& mesh) : _vertices(vertices), _mesh(mesh) {}
+    MeshFace(const std::vector<SP<MeshVertex>>& vertices, const SP<Mesh>& mesh) : _vertices(vertices), _mesh(mesh) {}
 
     auto mesh() const { return _mesh.lock(); }
     auto& vertices() const { return _vertices; }
-    std::vector<SP<Edge>> edges() const;
+    std::vector<SP<MeshEdge>> edges() const;
 
 private:
-    std::vector<SP<Vertex>> _vertices;
+    std::vector<SP<MeshVertex>> _vertices;
     WP<Mesh> _mesh;
 };
 
@@ -56,28 +56,28 @@ class Mesh : public std::enable_shared_from_this<Mesh> {
 public:
     Mesh();
 
-    SP<Vertex> addVertex();
-    SP<Edge> addEdge(const std::pair<SP<Vertex>, SP<Vertex>>& vertices);
-    SP<Face> addFace(const std::vector<SP<Vertex>>& vertices);
+    SP<MeshVertex> addVertex();
+    SP<MeshEdge> addEdge(const std::pair<SP<MeshVertex>, SP<MeshVertex>>& vertices);
+    SP<MeshFace> addFace(const std::vector<SP<MeshVertex>>& vertices);
 
     const auto& vertices() const { return _vertices; }
     const auto& edges() const { return _edges; }
     const auto& faces() const { return _faces; }
 
-    std::vector<SP<Edge>> edgesForVertex(const SP<Vertex>& vertex) const;
-    std::vector<SP<Face>> facesForVertex(const SP<Vertex>& vertex) const;
-    std::vector<SP<Edge>> edgesForFace(const SP<Face>& face) const;
-    std::vector<SP<Face>> facesForEdge(const SP<Edge>& edge) const;
+    std::vector<SP<MeshEdge>> edgesForVertex(const SP<MeshVertex>& vertex) const;
+    std::vector<SP<MeshFace>> facesForVertex(const SP<MeshVertex>& vertex) const;
+    std::vector<SP<MeshEdge>> edgesForFace(const SP<MeshFace>& face) const;
+    std::vector<SP<MeshFace>> facesForEdge(const SP<MeshEdge>& edge) const;
 
 private:
 
-    std::unordered_set<SP<Vertex>> _vertices;
-    std::unordered_map<std::pair<SP<Vertex>, SP<Vertex>>, SP<Edge>> _edges;
-    std::unordered_map<std::vector<SP<Vertex>>, SP<Face>> _faces;
-    std::unordered_multimap<SP<Vertex>, SP<Edge>> _edgesForVertex;
-    std::unordered_multimap<SP<Vertex>, SP<Face>> _facesForVertex;
-    std::unordered_multimap<SP<Face>, SP<Edge>> _edgesForFace;
-    std::unordered_multimap<SP<Edge>, SP<Face>> _facesForEdge;
+    std::unordered_set<SP<MeshVertex>> _vertices;
+    std::unordered_map<std::pair<SP<MeshVertex>, SP<MeshVertex>>, SP<MeshEdge>> _edges;
+    std::unordered_map<std::vector<SP<MeshVertex>>, SP<MeshFace>> _faces;
+    std::unordered_multimap<SP<MeshVertex>, SP<MeshEdge>> _edgesForVertex;
+    std::unordered_multimap<SP<MeshVertex>, SP<MeshFace>> _facesForVertex;
+    std::unordered_multimap<SP<MeshFace>, SP<MeshEdge>> _edgesForFace;
+    std::unordered_multimap<SP<MeshEdge>, SP<MeshFace>> _facesForEdge;
 };
 
 } // namespace Lattice
