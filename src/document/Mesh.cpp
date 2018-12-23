fa201dacbeaea5bd0a8d02fae26450b8b9eaf514
Ryohei Ikegami <iofg2100@gmail.com>
2018-12-24 01:59:37 +0900

    WIP Implement Mesh

--- src/document/Mesh.cpp
+++ src/document/Mesh.cpp
@@ -5,4 +5,45 @@ namespace Lattice {
 Mesh::Mesh() {
 }
 
+SP<Vertex> Mesh::addVertex() {
+    auto vertex = std::make_shared<Vertex>();
+    _vertices.insert(vertex);
+    return vertex;
+}
+
+SP<Edge> Mesh::addEdge(const std::pair<SP<Vertex>, SP<Vertex> > &vertices) {
+    auto edge = std::make_shared<Edge>(vertices, shared_from_this());
+    _edges[vertices] = edge;
+    _edgesForVertex.insert({vertices.first, edge});
+    _edgesForVertex.insert({vertices.second, edge});
+    return edge;
+}
+
+SP<Face> Mesh::addFace(const std::vector<SP<Vertex> > &vertices) {
+    auto face = std::make_shared<Face>(vertices, shared_from_this());
+    _faces[vertices] = face;
+    for (auto& v : vertices) {
+        _facesForVertex.insert({v, face});
+    }
+    return face;
+}
+
+std::vector<SP<Edge> > Mesh::edgesForVertex(const SP<Vertex> &vertex) const {
+    auto range = _edgesForVertex.equal_range(vertex);
+    std::vector<SP<Edge>> edges;
+    for (auto it = range.first; it != range.second; ++it) {
+        edges.push_back(it->second);
+    }
+    return edges;
+}
+
+std::vector<SP<Face> > Mesh::facesForVertex(const SP<Vertex> &vertex) const {
+    auto range = _facesForVertex.equal_range(vertex);
+    std::vector<SP<Face>> faces;
+    for (auto it = range.first; it != range.second; ++it) {
+        faces.push_back(it->second);
+    }
+    return faces;
+}
+
 } // namespace Lattice
--- src/document/Mesh.hpp
+++ src/document/Mesh.hpp
@@ -2,32 +2,78 @@
 #include <glm/glm.hpp>
 #include <vector>
 #include <unordered_map>
+#include <unordered_set>
 #include "../support/Hash.hpp"
+#include "../support/Pointer.hpp"
 
 namespace Lattice {
 
-class Mesh {
+class Vertex;
+class Edge;
+class Face;
+class Mesh;
+
+class Vertex {
 public:
-    struct VertexInfo {
-        glm::vec3 position;
-    };
+    SP<Mesh> mesh() const;
+    glm::vec3 position() const;
+    void setPosition(glm::vec3 position);
 
-    struct EdgeInfo {
-        // TODO
-    };
+    std::vector<SP<Edge>> edges() const;
+    std::vector<SP<Face>> faces() const;
 
-    struct FaceInfo {
-        // TODO
-    };
+private:
+    WP<Mesh> _mesh;
+};
 
+class Edge {
+public:
+    Edge(const std::pair<SP<Vertex>, SP<Vertex>>& vertices, const SP<Mesh>& mesh) : _vertices(vertices), _mesh(mesh) {}
+
+    auto mesh() const { return _mesh.lock(); }
+    auto& vertices() const { return _vertices; }
+    std::vector<SP<Face>> faces() const;
+
+private:
+    std::pair<SP<Vertex>, SP<Vertex>> _vertices;
+    WP<Mesh> _mesh;
+};
+
+class Face {
+public:
+    Face(const std::vector<SP<Vertex>>& vertices, const SP<Mesh>& mesh) : _vertices(vertices), _mesh(mesh) {}
+
+    auto mesh() const { return _mesh.lock(); }
+    auto& vertices() const { return _vertices; }
+    std::vector<SP<Edge>> edges() const;
+
+private:
+    std::vector<SP<Vertex>> _vertices;
+    WP<Mesh> _mesh;
+};
+
+class Mesh : public std::enable_shared_from_this<Mesh> {
+public:
     Mesh();
+
+    SP<Vertex> addVertex();
+    SP<Edge> addEdge(const std::pair<SP<Vertex>, SP<Vertex>>& vertices);
+    SP<Face> addFace(const std::vector<SP<Vertex>>& vertices);
+
+    const auto& vertices() const { return _vertices; }
+    const auto& edges() const { return _edges; }
+    const auto& faces() const { return _faces; }
+
+    std::vector<SP<Edge>> edgesForVertex(const SP<Vertex>& vertex) const;
+    std::vector<SP<Face>> facesForVertex(const SP<Vertex>& vertex) const;
 
 private:
 
-    std::vector<VertexInfo> _vertices;
-    std::unordered_map<std::pair<size_t, size_t>, EdgeInfo> _edges;
-    std::unordered_map<std::vector<size_t>, FaceInfo> _faces;
+    std::unordered_set<SP<Vertex>> _vertices;
+    std::unordered_map<std::pair<SP<Vertex>, SP<Vertex>>, SP<Edge>> _edges;
+    std::unordered_map<std::vector<SP<Vertex>>, SP<Face>> _faces;
+    std::unordered_multimap<SP<Vertex>, SP<Edge>> _edgesForVertex;
+    std::unordered_multimap<SP<Vertex>, SP<Face>> _facesForVertex;
 };
 
 } // namespace Lattice
-
