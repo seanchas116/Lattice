c3ee419e4fc4ff9495fbc376cb5710abf3528af9
Ryohei Ikegami <iofg2100@gmail.com>
2018-12-25 20:25:14 +0900

    Reflect document selection to selection model

--- src/app/ItemSelectionModel.cpp
+++ src/app/ItemSelectionModel.cpp
@@ -7,6 +7,28 @@ namespace Lattice {
 
 ItemSelectionModel::ItemSelectionModel(ItemModel *model, QObject *parent) : QItemSelectionModel(model, parent)
 {
+    auto onSelectionChange = [this, model] {
+        QItemSelection selection;
+        for (auto item : model->document()->selectedItems()) {
+            auto index = model->indexForItem(item);
+            selection.select(index, index);
+        }
+        select(selection, QItemSelectionModel::ClearAndSelect);
+    };
+    auto onCurrentChange = [this, model] {
+        auto currentItem = model->document()->currentItem();
+        if (currentItem) {
+            auto current = model->indexForItem(currentItem);
+            select(current, QItemSelectionModel::Current);
+        } else {
+            clearCurrentIndex();
+        }
+    };
+    connect(model->document().get(), &Document::selectedItemsChanged, this, onSelectionChange);
+    connect(model->document().get(), &Document::currentItemChanged, this, onCurrentChange);
+    onSelectionChange();
+    onCurrentChange();
+
     connect(this, &QItemSelectionModel::selectionChanged, model, [this, model] {
         std::unordered_set<SP<Item>> items;
         for (auto index : selectedIndexes()) {
