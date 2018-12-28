a25f9206fe06d07e754778e7c09a356a7accede1
Ryohei Ikegami <iofg2100@gmail.com>
2018-12-29 01:01:46 +0900

    Load material correctly

--- src/services/ObjLoader.cpp
+++ src/services/ObjLoader.cpp
@@ -2,17 +2,21 @@
 #include "../document/Mesh.hpp"
 #include "../document/MeshItem.hpp"
 #include <tiny_obj_loader.h>
+#include <boost/filesystem.hpp>
 #include <QtDebug>
 
 namespace Lattice {
 
-std::vector<SP<MeshItem>> ObjLoader::load(const std::string &filePath) {
+std::vector<SP<MeshItem>> ObjLoader::load(const std::string &filePathString) {
     tinyobj::attrib_t attrib;
     std::vector<tinyobj::shape_t> objShapes;
     std::vector<tinyobj::material_t> objMaterials;
 
+    boost::filesystem::path filePath(filePathString);
+    auto parentPathString = filePath.parent_path().string() + "/";
+
     std::string err;
-    bool ret = tinyobj::LoadObj(&attrib, &objShapes, &objMaterials, &err, filePath.c_str());
+    bool ret = tinyobj::LoadObj(&attrib, &objShapes, &objMaterials, &err, filePathString.c_str(), parentPathString.c_str());
 
     if (!err.empty()) {
         qWarning() << err.c_str();
