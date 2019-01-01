6ca7ebab2f45ad590f20dffc9abed84df7edc8e5
Ryohei Ikegami <iofg2100@gmail.com>
2019-01-01 12:33:11 +0900

    Use double in Line

--- src/support/Line.cpp
+++ src/support/Line.cpp
@@ -4,33 +4,33 @@ using namespace glm;
 
 namespace Lattice {
 
-LinePointDistance::LinePointDistance(const Line &l, const glm::vec3 p) {
+LinePointDistance::LinePointDistance(const Line &l, const dvec3 p) {
     // http://geomalgorithms.com/a02-_lines.html#Distance-to-Infinite-Line
-    glm::vec3 w = p - l.a;
-    glm::vec3 v = glm::normalize(l.b - l.a);
+    dvec3 w = p - l.a;
+    dvec3 v = glm::normalize(l.b - l.a);
     distance = length(cross(v, w));
     t = dot(v, w);
 }
 
 LineLineDistance::LineLineDistance(const Line &l0, const Line &l1) {
     // http://geomalgorithms.com/a07-_distance.html
-    vec3 u = l0.b - l0.a;
-    vec3 v = l1.b - l1.a;
-    vec3 w0 = l0.a - l1.a;
+    dvec3 u = l0.b - l0.a;
+    dvec3 v = l1.b - l1.a;
+    dvec3 w0 = l0.a - l1.a;
 
-    float a = dot(u, u);
-    float b = dot(u, v);
-    float c = dot(v, v);
-    float d = dot(u, w0);
-    float e = dot(v, w0);
+    double a = dot(u, u);
+    double b = dot(u, v);
+    double c = dot(v, v);
+    double d = dot(u, w0);
+    double e = dot(v, w0);
 
-    float denom = a * c - b * b;
+    double denom = a * c - b * b;
 
     t0 = (b * e - c * d) / denom;
     t1 = (a * e - b * d) / denom;
 
-    vec3 p = mix(l0.a, l0.b, t0);
-    vec3 q = mix(l1.a, l1.b, t1);
+    dvec3 p = mix(l0.a, l0.b, t0);
+    dvec3 q = mix(l1.a, l1.b, t1);
 
     distance = glm::distance(p, q);
 }
