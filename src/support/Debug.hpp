7b33a5650207bf87e1aafe66c386fa996deeee1c
Ryohei Ikegami <iofg2100@gmail.com>
2019-01-01 13:14:05 +0900

    Support any vec in Debug

--- src/support/Debug.hpp
+++ src/support/Debug.hpp
@@ -7,16 +7,28 @@
 
 namespace Lattice {
 
-QDebug operator<<(QDebug debug, const glm::vec2& v);
-QDebug operator<<(QDebug debug, const glm::vec3& v);
-QDebug operator<<(QDebug debug, const glm::vec4& v);
+template<glm::length_t L, typename T, glm::qualifier Q> QDebug operator<<(QDebug debug, const glm::vec<L, T, Q>& v) {
+    QDebugStateSaver saver(debug);
+    debug.nospace();
+    debug << "(";
+    for (size_t i = 0; i < L; ++i) {
+        if (i != 0) {
+            debug << ", ";
+        }
+        debug << v[i];
+    }
+    debug << ")";
+    return debug;
+}
 
 template <typename T, size_t N>
 QDebug operator<<(QDebug debug, const std::array<T, N>& array) {
+    QDebugStateSaver saver(debug);
+    debug.nospace();
     debug << "[";
     for (size_t i = 0; i < array.size(); ++i) {
         if (i != 0) {
-            debug << ",";
+            debug << ", ";
         }
         debug << array[i];
     }
