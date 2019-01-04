e3c8c50d461d50a00104306d2076bf3e5d080b65
Ryohei Ikegami <iofg2100@gmail.com>
2019-01-05 00:55:24 +0900

    Implement Ray::intersectsTriangle

--- src/support/Ray.hpp
+++ src/support/Ray.hpp
@@ -2,6 +2,7 @@
 
 #include <glm/glm.hpp>
 #include <tuple>
+#include <array>
 
 namespace Lattice {
 
@@ -13,6 +14,36 @@ public:
 
     template <typename U>
     Ray(const Ray<U>& other) : origin(other.origin), direction(other.direction) {}
+
+    bool intersectsTriangle(const std::array<glm::tvec3<T>, 3>& triangle) const {
+        // https://en.wikipedia.org/wiki/M%C3%B6ller%E2%80%93Trumbore_intersection_algorithm
+        using namespace glm;
+        const T EPSILON = 0.0000001;
+        auto [vertex0, vertex1, vertex2] = triangle;
+        tvec3<T> edge1, edge2, h, s, q;
+        T a,f,u,v;
+        edge1 = vertex1 - vertex0;
+        edge2 = vertex2 - vertex0;
+        h = cross(direction, edge2);
+        a = dot(edge1, h);
+        if (a > -EPSILON && a < EPSILON) {
+            return false;    // This ray is parallel to this triangle.
+        }
+        f = 1/a;
+        s = origin - vertex0;
+        u = f * (dot(s, h));
+        if (u < 0.0 || u > 1.0) {
+            return false;
+        }
+        q = cross(s, edge1);
+        v = f * dot(direction, q);
+        if (v < 0.0 || u + v > 1.0) {
+            return false;
+        }
+        // At this stage we can compute t to find out where the intersection point is on the line.
+        T t = f * dot(edge2, q);
+        return t > EPSILON;
+    }
 
     glm::tvec3<T> origin {0};
     glm::tvec3<T> direction {0};
