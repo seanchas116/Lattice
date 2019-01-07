34821e64e2448dba8cc45e2c039933d9c0ac9624
Ryohei Ikegami <iofg2100@gmail.com>
2019-01-07 16:11:43 +0900

    Sort intersectings

--- src/viewport/MeshPicker.cpp
+++ src/viewport/MeshPicker.cpp
@@ -8,9 +8,9 @@ MeshPicker::MeshPicker(const SP<Document::Mesh> &mesh) : _mesh(mesh) {
 }
 
 std::pair<SP<Document::MeshFace>, double> MeshPicker::picKFace(const Ray<double> &ray) const {
-    ScopedTimer timer("MeshPicker::pickFace");
     // TODO: Use Bounding Volume Hierarchy to do faster
-    // TODO: sort intersectings
+    ScopedTimer timer("MeshPicker::pickFace");
+    std::map<float, SP<Document::MeshFace>> intersectings;
     for (auto& [_, f] : _mesh->faces()) {
         auto v0 = f->vertices()[0]->position();
         for (uint32_t i = 2; i < uint32_t(f->vertices().size()); ++i) {
@@ -19,11 +19,15 @@ std::pair<SP<Document::MeshFace>, double> MeshPicker::picKFace(const Ray<double>
 
             auto [intersects, t] = ray.intersectsTriangle({v0, v1, v2});
             if (intersects) {
-                return {f, t};
+                intersectings[t] = f;
             }
         }
     }
-    return {{}, 0};
+    if (intersectings.empty()) {
+        return {};
+    }
+    auto nearest = intersectings.begin();
+    return {nearest->second, nearest->first};
 }
 
 std::pair<SP<Document::MeshVertex>, double> MeshPicker::pickVertex(const Ray<double> &ray, double distance) const {
