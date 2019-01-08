63f5140c7ca56a091b62e7d2cea52a507d1e5be9
Ryohei Ikegami <iofg2100@gmail.com>
2019-01-08 21:18:31 +0900

    Add EditorWidget

---
+++ src/editor/EditorWidget.cpp
@@ -0,0 +1,53 @@
+#include "EditorWidget.hpp"
+#include "ViewportRenderer.hpp"
+#include "CameraController.hpp"
+#include <QOpenGLDebugLogger>
+#include <QMouseEvent>
+
+namespace Lattice::Editor {
+
+EditorWidget::EditorWidget(const SP<UI::AppState> &appState, QWidget *parent) :
+    Render::RenderWidget(parent),
+    _appState(appState)
+{
+    setFocusPolicy(Qt::ClickFocus);
+    connect(&_keyObserver, &KeyObserver::selectedKeysChanged, &_cameraController, &CameraController::setPressedKeys);
+}
+
+void EditorWidget::mousePressEvent(QMouseEvent *event) {
+    if (_cameraController.mousePress(event)) {
+        return;
+    }
+    RenderWidget::mousePressEvent(event);
+}
+
+void EditorWidget::mouseMoveEvent(QMouseEvent *event) {
+    if (_cameraController.mouseMove(event)) {
+        return;
+    }
+    RenderWidget::mouseMoveEvent(event);
+}
+
+void EditorWidget::mouseReleaseEvent(QMouseEvent *event) {
+    if (_cameraController.mouseRelease(event)) {
+        return;
+    }
+    RenderWidget::mouseReleaseEvent(event);
+}
+
+void EditorWidget::wheelEvent(QWheelEvent *event) {
+    _cameraController.wheel(event);
+    RenderWidget::wheelEvent(event);
+}
+
+void EditorWidget::keyPressEvent(QKeyEvent *event) {
+    _keyObserver.keyPress(event);
+    RenderWidget::keyPressEvent(event);
+}
+
+void EditorWidget::keyReleaseEvent(QKeyEvent *event) {
+    _keyObserver.keyRelease(event);
+    RenderWidget::keyReleaseEvent(event);
+}
+
+} // namespace Lattice
---
+++ src/editor/EditorWidget.hpp
@@ -0,0 +1,36 @@
+#pragma once
+#include <QOpenGLWidget>
+#include <memory>
+#include "CameraController.hpp"
+#include "KeyObserver.hpp"
+#include "../support/Pointer.hpp"
+#include "../render/RenderWidget.hpp"
+
+namespace Lattice::UI {
+class AppState;
+}
+
+namespace Lattice::Editor {
+
+class EditorWidget final : public Render::RenderWidget {
+    Q_OBJECT
+public:
+    explicit EditorWidget(const SP<UI::AppState>& appState, QWidget *parent = nullptr);
+
+protected:
+    void mousePressEvent(QMouseEvent *event) override;
+    void mouseMoveEvent(QMouseEvent *event) override;
+    void mouseReleaseEvent(QMouseEvent *event) override;
+
+    void wheelEvent(QWheelEvent *event) override;
+
+    void keyPressEvent(QKeyEvent *event) override;
+    void keyReleaseEvent(QKeyEvent *event) override;
+
+private:
+    CameraController _cameraController;
+    KeyObserver _keyObserver;
+    SP<UI::AppState> _appState;
+};
+
+} // namespace Lattice
