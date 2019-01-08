ab5dbaf79480032d04e0ed4ac242cf4d641309d0
Ryohei Ikegami <iofg2100@gmail.com>
2019-01-08 18:04:22 +0900

    Add Operations

---
+++ src/renderer/Operations.hpp
@@ -0,0 +1,22 @@
+#pragma once
+
+#include "./operations/DrawCircle.hpp"
+#include "./operations/DrawLine.hpp"
+#include "./operations/DrawSolid.hpp"
+#include "./operations/DrawMaterial.hpp"
+
+namespace Lattice {
+namespace Renderer {
+
+class Operations final {
+public:
+    Operations() = default;
+
+    DrawCircle drawCircle;
+    DrawLine drawLine;
+    DrawSolid drawSolid;
+    DrawMaterial drawMaterial;
+};
+
+}
+}
