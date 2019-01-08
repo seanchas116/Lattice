2b1630668904de43c310cb5784259e4ca9022d35
Ryohei Ikegami <iofg2100@gmail.com>
2019-01-08 22:28:23 +0900

    Add more renderables to EditorScene

--- src/editor/EditorScene.cpp
+++ src/editor/EditorScene.cpp
@@ -1,13 +1,23 @@
 #include "EditorScene.hpp"
-#include "GridFloor.hpp"
 #include "Background.hpp"
+#include "GridFloor.hpp"
+#include "MeshRenderer.hpp"
+#include "Manipulator.hpp"
+#include "ManipulatorController.hpp"
+#include "ItemPicker.hpp"
+#include "ItemInteractor.hpp"
 
 namespace Lattice {
 namespace Editor {
 
-EditorScene::EditorScene() :
+EditorScene::EditorScene(const SP<UI::AppState> &appState) :
+    _appState(appState),
     _background(makeShared<Background>()),
-    _gridFloor(makeShared<GridFloor>())
+    _gridFloor(makeShared<GridFloor>()),
+    _manipulator(makeShared<Manipulator>()),
+    _manipulatorController(makeShared<ManipulatorController>(_manipulator, appState)),
+    _itemPicker(makeShared<ItemPicker>()),
+    _itemInteractor(makeShared<ItemInteractor>(_itemPicker))
 {
 }
 
@@ -15,6 +25,7 @@ void EditorScene::updateLayers() {
     _layers = {
         {_background},
         {_gridFloor},
+        {_manipulator},
     };
 }
 
--- src/editor/EditorScene.hpp
+++ src/editor/EditorScene.hpp
@@ -3,15 +3,30 @@
 #include "../render/RenderWidget.hpp"
 
 namespace Lattice {
+
+namespace UI {
+class AppState;
+}
+
+namespace Document {
+class MeshItem;
+}
+
 namespace Editor {
 
 class Background;
 class GridFloor;
+class MeshRenderer;
+class Operations;
+class Manipulator;
+class ManipulatorController;
+class ItemPicker;
+class ItemInteractor;
 
 class EditorScene : public QObject {
     Q_OBJECT
 public:
-    EditorScene();
+    EditorScene(const SP<UI::AppState>& appState);
 
     auto& layers() const { return _layers; }
 
@@ -21,9 +36,17 @@ signals:
     void updateRequested();
 
 private:
-    std::vector<Render::Layer> _layers;
+    SP<UI::AppState> _appState;
+
     SP<Background> _background;
     SP<GridFloor> _gridFloor;
+    SP<Manipulator> _manipulator;
+    SP<ManipulatorController> _manipulatorController;
+    std::unordered_map<SP<Document::MeshItem>, SP<MeshRenderer>> _meshRenderers;
+    SP<ItemPicker> _itemPicker;
+    SP<ItemInteractor> _itemInteractor;
+
+    std::vector<Render::Layer> _layers;
 };
 
 } // namespace Editor
