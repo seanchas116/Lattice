8b92b080edd367c01b5c7033f4f0aa17c8788a4c
Ryohei Ikegami <iofg2100@gmail.com>
2019-01-08 22:28:12 +0900

    Enable depth test in Background

--- src/editor/Background.cpp
+++ src/editor/Background.cpp
@@ -9,6 +9,11 @@ Background::Background() {
 
 void Background::draw(const SP<Render::Operations> &operations, const Camera &camera) {
     Q_UNUSED(operations); Q_UNUSED(camera);
+
+    // TODO: manage depth test in better way
+    glEnable(GL_DEPTH_TEST);
+    glDepthFunc(GL_LESS);
+
     glClearDepthf(1.f);
     glClearColor(0.8f, 0.8f, 0.8f, 1);
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
