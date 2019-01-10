07df3a9ef44b553a45c1f7c2b218cd459a519e29
Ryohei Ikegami <iofg2100@gmail.com>
2019-01-11 01:06:50 +0900

    Implement dragging in MeshRenderer

--- src/editor/MeshRenderer.cpp
+++ src/editor/MeshRenderer.cpp
@@ -5,10 +5,12 @@
 #include "../gl/LineVAO.hpp"
 #include "../gl/PointVAO.hpp"
 #include "../document/Document.hpp"
+#include "../document/History.hpp"
 #include "../document/Mesh.hpp"
 #include "../document/MeshItem.hpp"
 #include "../support/Debug.hpp"
 #include "../support/Camera.hpp"
+#include <QMouseEvent>
 
 using namespace glm;
 
@@ -37,6 +39,45 @@ void MeshRenderer::draw(const SP<Render::Operations> &operations, const Camera &
     if (_appState->isVertexVisible()) {
         operations->drawCircle.draw(_vertexVAO, _item->location().matrix(), camera, 4.0, dvec3(0));
     }
+}
+
+std::pair<bool, double> MeshRenderer::mousePress(QMouseEvent *event, dvec2 pos, const Camera &camera) {
+    auto worldMouseRay = camera.worldMouseRay(pos);
+    LATTICE_OPTIONAL_GUARD(pickResult, _meshPicker->picKFace(worldMouseRay), return {true, 0};)
+    auto [face, t] = pickResult;
+
+    glm::dvec3 worldDragPos = worldMouseRay.at(t);
+    auto [screenDragPos, isInScreen] = camera.mapWorldToScreen(worldDragPos);
+    if (!isInScreen) {
+        return {false, 0};
+    }
+
+    _dragInitLocation = _item->location();
+    _dragInitWorldPos = worldDragPos;
+    _dragInitDepth = screenDragPos.z;
+    _dragStarted = false;
+
+    _appState->document()->selectItem(_item, event->modifiers() & Qt::ShiftModifier);
+
+    return {true, 0};
+}
+
+void MeshRenderer::mouseMove(QMouseEvent *event, dvec2 pos, const Camera &camera) {
+    Q_UNUSED(event);
+
+    auto newWorldPos = camera.mapScreenToWorld(glm::dvec3(pos, _dragInitDepth));
+    auto newLocation = _dragInitLocation;
+    newLocation.position += newWorldPos - _dragInitWorldPos;
+
+    if (!_dragStarted) {
+        _appState->document()->history()->beginChange(tr("Move Item"));
+        _dragStarted = true;
+    }
+    _item->setLocation(newLocation);
+}
+
+void MeshRenderer::mouseRelease(QMouseEvent *event, dvec2 pos, const Camera &camera) {
+    Q_UNUSED(event); Q_UNUSED(pos); Q_UNUSED(camera);
 }
 
 void MeshRenderer::updateVAOs(const SP<Document::Mesh> &mesh) {
--- src/editor/MeshRenderer.hpp
+++ src/editor/MeshRenderer.hpp
@@ -2,6 +2,7 @@
 
 #include "../support/Pointer.hpp"
 #include "../support/Box.hpp"
+#include "../support/Location.hpp"
 #include "../render/Renderable.hpp"
 #include <glm/glm.hpp>
 #include <unordered_map>
@@ -32,11 +33,16 @@ namespace Editor {
 
 class MeshPicker;
 
-class MeshRenderer final : public Render::Renderable {
+class MeshRenderer final : public QObject, public Render::Renderable {
+    Q_OBJECT
 public:
     MeshRenderer(const SP<UI::AppState>& appState, const SP<Document::MeshItem>& item);
 
     void draw(const SP<Render::Operations> &operations, const Camera &camera) override;
+
+    std::pair<bool, double> mousePress(QMouseEvent *event, glm::dvec2 pos, const Camera &camera) override;
+    void mouseMove(QMouseEvent *event, glm::dvec2 pos, const Camera &camera) override;
+    void mouseRelease(QMouseEvent *event, glm::dvec2 pos, const Camera &camera) override;
 
 private:
     void updateVAOs(const SP<Document::Mesh>& mesh);
@@ -47,6 +53,11 @@ private:
     std::unordered_map<SP<Document::MeshMaterial>, SP<GL::VAO>> _faceVAOs;
     SP<GL::LineVAO> _edgeVAO;
     SP<GL::PointVAO> _vertexVAO;
+
+    Location _dragInitLocation;
+    glm::dvec3 _dragInitWorldPos;
+    double _dragInitDepth;
+    bool _dragStarted;
 };
 
 }
