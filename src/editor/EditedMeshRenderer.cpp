70b59eda5ea940c724978df80093a6435e3b7696
Ryohei Ikegami <iofg2100@gmail.com>
2019-01-13 17:44:58 +0900

    Add EditedMeshRenderer

---
+++ src/editor/EditedMeshRenderer.cpp
@@ -0,0 +1,92 @@
+#include "EditedMeshRenderer.hpp"
+#include "MeshVAOGenerator.hpp"
+#include "MeshPicker.hpp"
+#include "../ui/AppState.hpp"
+#include "../gl/LineVAO.hpp"
+#include "../gl/PointVAO.hpp"
+#include "../document/Document.hpp"
+#include "../document/History.hpp"
+#include "../document/Mesh.hpp"
+#include "../document/MeshItem.hpp"
+#include "../support/Debug.hpp"
+#include "../support/Camera.hpp"
+#include <QMouseEvent>
+
+using namespace glm;
+
+namespace Lattice::Editor {
+
+EditedMeshRenderer::EditedMeshRenderer(const SP<UI::AppState>& appState, const SP<Document::MeshItem> &item) :
+    _appState(appState),
+    _item(item),
+    _meshPicker(makeShared<MeshPicker>(item->mesh())),
+    _edgeVAO(makeShared<GL::LineVAO>()),
+    _vertexVAO(makeShared<GL::PointVAO>())
+{
+    // TODO: update mesh when item is changed
+    updateVAOs(item->mesh());
+}
+
+void EditedMeshRenderer::draw(const SP<Render::Operations> &operations, const Camera &camera) {
+    if (_appState->isFaceVisible()) {
+        for (auto& [material, vao] : _faceVAOs) {
+            operations->drawMaterial.draw(vao, _item->location().matrix(), camera, material);
+        }
+    }
+    if (_appState->isEdgeVisible()) {
+        operations->drawLine.draw(_edgeVAO, _item->location().matrix(), camera, 1.0, dvec3(0));
+    }
+    if (_appState->isVertexVisible()) {
+        operations->drawCircle.draw(_vertexVAO, _item->location().matrix(), camera, 4.0, dvec3(0));
+    }
+}
+
+std::optional<Render::HitResult> EditedMeshRenderer::hitTest(dvec2 pos, const Camera &camera) const {
+    auto worldMouseRay = camera.worldMouseRay(pos);
+    auto modelMouseRay = glm::inverse(_item->location().matrix()) * worldMouseRay;
+    LATTICE_OPTIONAL_GUARD(pickResult, _meshPicker->picKFace(modelMouseRay), return {};)
+    auto [face, t] = pickResult;
+    return {{t}};
+}
+
+void EditedMeshRenderer::mousePress(QMouseEvent *event, dvec2 pos, const Camera &camera, const Render::HitResult& hitResult) {
+    glm::dvec3 worldDragPos = camera.worldMouseRay(pos).at(hitResult.t);
+    auto [screenDragPos, isInScreen] = camera.mapWorldToScreen(worldDragPos);
+    if (!isInScreen) {
+        return;
+    }
+
+    _dragInitLocation = _item->location();
+    _dragInitWorldPos = worldDragPos;
+    _dragInitDepth = screenDragPos.z;
+    _dragStarted = false;
+
+    _appState->document()->selectItem(_item, event->modifiers() & Qt::ShiftModifier);
+}
+
+void EditedMeshRenderer::mouseMove(QMouseEvent *event, dvec2 pos, const Camera &camera) {
+    Q_UNUSED(event);
+
+    auto newWorldPos = camera.mapScreenToWorld(glm::dvec3(pos, _dragInitDepth));
+    auto newLocation = _dragInitLocation;
+    newLocation.position += newWorldPos - _dragInitWorldPos;
+
+    if (!_dragStarted) {
+        _appState->document()->history()->beginChange(tr("Move Item"));
+        _dragStarted = true;
+    }
+    _item->setLocation(newLocation);
+}
+
+void EditedMeshRenderer::mouseRelease(QMouseEvent *event, dvec2 pos, const Camera &camera) {
+    Q_UNUSED(event); Q_UNUSED(pos); Q_UNUSED(camera);
+}
+
+void EditedMeshRenderer::updateVAOs(const SP<Document::Mesh> &mesh) {
+    MeshVAOGenerator generator(mesh);
+    _vertexVAO = generator.generateVertexVAO();
+    _edgeVAO = generator.generateEdgeVAO();
+    _faceVAOs= generator.generateFaceVAOs();
+}
+
+}
---
+++ src/editor/EditedMeshRenderer.hpp
@@ -0,0 +1,65 @@
+#pragma once
+
+#include "../support/Pointer.hpp"
+#include "../support/Box.hpp"
+#include "../support/Location.hpp"
+#include "../render/Renderable.hpp"
+#include <glm/glm.hpp>
+#include <unordered_map>
+
+class QMouseEvent;
+
+namespace Lattice {
+
+class Camera;
+
+namespace UI {
+class AppState;
+}
+
+namespace Document {
+class MeshItem;
+class Mesh;
+class MeshMaterial;
+}
+
+namespace GL {
+class VAO;
+class LineVAO;
+class PointVAO;
+}
+
+namespace Editor {
+
+class MeshPicker;
+
+class EditedMeshRenderer final : public QObject, public Render::Renderable {
+    Q_OBJECT
+public:
+    EditedMeshRenderer(const SP<UI::AppState>& appState, const SP<Document::MeshItem>& item);
+
+    void draw(const SP<Render::Operations> &operations, const Camera &camera) override;
+
+    std::optional<Render::HitResult> hitTest(glm::dvec2 pos, const Camera& camera) const override;
+    void mousePress(QMouseEvent *event, glm::dvec2 pos, const Camera &camera, const Render::HitResult& hitResult) override;
+    void mouseMove(QMouseEvent *event, glm::dvec2 pos, const Camera &camera) override;
+    void mouseRelease(QMouseEvent *event, glm::dvec2 pos, const Camera &camera) override;
+
+private:
+    void updateVAOs(const SP<Document::Mesh>& mesh);
+
+    SP<UI::AppState> _appState;
+    SP<Document::MeshItem> _item;
+    SP<MeshPicker> _meshPicker;
+    std::unordered_map<SP<Document::MeshMaterial>, SP<GL::VAO>> _faceVAOs;
+    SP<GL::LineVAO> _edgeVAO;
+    SP<GL::PointVAO> _vertexVAO;
+
+    Location _dragInitLocation;
+    glm::dvec3 _dragInitWorldPos;
+    double _dragInitDepth;
+    bool _dragStarted;
+};
+
+}
+}
