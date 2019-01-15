d96d256b8659cb2405b414fcdc8f8922af0678ab
Ryohei Ikegami <iofg2100@gmail.com>
2019-01-15 17:52:54 +0900

    Add MeshSelection median position

--- src/document/MeshSelection.cpp
+++ src/document/MeshSelection.cpp
@@ -1,7 +1,26 @@
 #include "MeshSelection.hpp"
 
+using namespace glm;
+
 namespace Lattice {
 namespace Document {
+
+glm::vec3 MeshSelection::medianPosition() const {
+    if (vertices.empty()) {
+        return vec3(0);
+    }
+
+    vec3 minPos(INFINITY);
+    vec3 maxPos(-INFINITY);
+
+    for (auto& v : vertices) {
+        auto p = v->position();
+        minPos = min(minPos, p);
+        maxPos = max(maxPos, p);
+    }
+
+    return (minPos + maxPos) / 2.f;
+}
 
 } // namespace Document
 } // namespace Lattice
--- src/document/MeshSelection.hpp
+++ src/document/MeshSelection.hpp
@@ -7,9 +7,12 @@ namespace Document {
 class MeshSelection {
 public:
     std::unordered_set<SP<MeshVertex>> vertices;
+
     bool operator==(const MeshSelection& other) const {
         return vertices == other.vertices;
     }
+
+    glm::vec3 medianPosition() const;
 };
 
 } // namespace Document
