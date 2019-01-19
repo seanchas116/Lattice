da84b50ee2be542ac525a4560191d7b1038a4df5
Ryohei Ikegami <iofg2100@gmail.com>
2019-01-20 01:58:52 +0900

    Remove direct relation between face and vertex

--- src/document/Mesh.hpp
+++ src/document/Mesh.hpp
@@ -40,7 +40,6 @@ private:
     glm::vec3 _position;
     glm::vec2 _texCoord;
     std::unordered_set<MeshEdge*> _edges;
-    std::unordered_set<MeshFace*> _faces;
     std::unordered_set<SP<MeshUVPoint>> _uvPoints;
 };
 
@@ -98,11 +97,11 @@ private:
 
 class MeshFace final : public EnableSharedFromThis<MeshFace> {
 public:
-    MeshFace(const std::vector<SP<MeshVertex>>& vertices, const std::vector<SP<MeshEdge>>& edges,
+    MeshFace(const std::vector<SP<MeshEdge>>& edges,
              const std::vector<SP<MeshUVPoint>>& uvPoints, const std::vector<SP<MeshUVEdge>>& uvEdges,
              const SP<MeshMaterial>& material);
 
-    auto& vertices() const { return _vertices; }
+    std::vector<SP<MeshVertex>> vertices() const;
     auto& edges() const { return _edges; }
     auto& uvPoints() const { return _uvPoints; }
     auto& uvEdges() const { return _uvEdges; }
@@ -114,7 +113,6 @@ public:
 
 private:
     friend class Mesh;
-    std::vector<SP<MeshVertex>> _vertices;
     std::vector<SP<MeshEdge>> _edges;
     std::vector<SP<MeshUVPoint>> _uvPoints;
     std::vector<SP<MeshUVEdge>> _uvEdges;
