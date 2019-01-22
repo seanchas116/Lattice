2d83177161515421538811140651b0c92e1bff8a
Ryohei Ikegami <iofg2100@gmail.com>
2019-01-22 14:42:04 +0900

    Add ViewportContainer

---
+++ src/render/ViewportContainer.hpp
@@ -0,0 +1,41 @@
+#pragma once
+#include <QOpenGLWidget>
+#include "../support/Pointer.hpp"
+#include "Operations.hpp"
+#include "MouseEvent.hpp"
+
+namespace Lattice {
+namespace Render {
+
+class Renderable;
+class Viewport;
+
+class ViewportContainer : public QOpenGLWidget, protected QOpenGLExtraFunctions {
+    Q_OBJECT
+public:
+    ViewportContainer(QWidget* parent = nullptr);
+
+    auto& viewports() const { return _viewports; }
+    void setViewports(const std::vector<SP<Viewport>>& viewports) { _viewports = viewports; }
+
+    glm::dvec2 mapQtToGL(const QPoint& p) const;
+
+signals:
+    void resized();
+    void initialized();
+    void aboutToBePainted();
+
+protected:
+    void initializeGL() override;
+    void resizeGL(int w, int h) override;
+    void paintGL() override;
+
+private:
+    double widgetPixelRatio() const;
+
+    std::optional<SP<Operations>> _operations;
+    std::vector<SP<Viewport>> _viewports;
+};
+
+} // namespace Renderer
+} // namespace Lattice
