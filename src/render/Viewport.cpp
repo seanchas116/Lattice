6038529a004396cba6ff6bfb4aaa823735433614
Ryohei Ikegami <iofg2100@gmail.com>
2019-01-22 14:25:58 +0900

    Add Viewport

--- src/render/Viewport.cpp
+++ src/render/Viewport.cpp
@@ -1,7 +1,86 @@
 #include "Viewport.hpp"
+#include "Renderable.hpp"
+#include "RenderWidget.hpp"
+#include "../support/Debug.hpp"
+#include <QMouseEvent>
+#include <QOpenGLDebugLogger>
 
 namespace Lattice {
 namespace Render {
+
+Viewport::Viewport(QWidget *parent) : QWidget(parent) {}
+
+void Viewport::mousePressEvent(QMouseEvent *event) {
+    auto pos = mapQtToGL(event->pos());
+
+    auto maybeHitResult = hitTest(pos, _camera);
+    if (!maybeHitResult) { return; }
+
+    auto [renderable, hitResult] = *maybeHitResult;
+
+    MouseEvent renderMouseEvent(event, pos, _camera, hitResult);
+    renderable->mousePress(renderMouseEvent);
+    _draggedRenderable = renderable;
+    _hitResult = hitResult;
+}
+
+void Viewport::mouseMoveEvent(QMouseEvent *event) {
+    LATTICE_OPTIONAL_GUARD(renderable, _draggedRenderable, return;)
+    MouseEvent renderMouseEvent(event, mapQtToGL(event->pos()), _camera, _hitResult);
+    renderable->mouseMove(renderMouseEvent);
+}
+
+void Viewport::mouseDoubleClickEvent(QMouseEvent *event) {
+    auto pos = mapQtToGL(event->pos());
+
+    auto maybeHitResult = hitTest(pos, _camera);
+    if (!maybeHitResult) { return; }
+
+    auto [renderable, hitResult] = *maybeHitResult;
+
+    MouseEvent renderMouseEvent(event, pos, _camera, hitResult);
+    renderable->mouseDoubleClick(renderMouseEvent);
+}
+
+void Viewport::mouseReleaseEvent(QMouseEvent *event) {
+    LATTICE_OPTIONAL_GUARD(renderable, _draggedRenderable, return;)
+    MouseEvent renderMouseEvent(event, mapQtToGL(event->pos()), _camera, _hitResult);
+    renderable->mouseRelease(renderMouseEvent);
+    _draggedRenderable = {};
+}
+
+std::optional<std::pair<SP<Renderable>, HitResult> > Viewport::hitTest(glm::dvec2 pos, const Camera &camera) {
+    std::map<double, std::pair<SP<Renderable>, HitResult>> hitRenderables;
+    for (auto it = _renderables.rbegin(); it != _renderables.rend(); ++it) {
+        auto& renderable = *it;
+        auto maybeHitResult = renderable->hitTest(pos, camera);
+        if (maybeHitResult) {
+            hitRenderables.insert({maybeHitResult->depth, {renderable, *maybeHitResult}});
+        }
+    }
+    if (hitRenderables.empty()) {
+        return std::nullopt;
+    }
+    return hitRenderables.begin()->second;
+}
+
+void Viewport::render(const SP<Operations> &operations) {
+    for (auto& renderable : _renderables) {
+        renderable->draw(operations, _camera);
+    }
+}
+
+glm::dvec2 Viewport::mapQtToGL(const QPoint &p) const {
+    return glm::dvec2(p.x(), height() - p.y()) / widgetPixelRatio();
+}
+
+double Viewport::widgetPixelRatio() const {
+#ifdef Q_OS_WIN
+    return logicalDpiX() / 96.0;
+#else
+    return 1.0;
+#endif
+}
 
 } // namespace Render
 } // namespace Lattice
--- src/render/Viewport.hpp
+++ src/render/Viewport.hpp
@@ -1,17 +1,42 @@
 #pragma once
-#include <QObject>
+#include <QOpenGLWidget>
+#include "../support/Pointer.hpp"
+#include "Operations.hpp"
+#include "MouseEvent.hpp"
 
 namespace Lattice {
 namespace Render {
 
-class Viewport : public QObject {
+class Renderable;
+class RenderWidget;
+
+class Viewport : public QWidget, protected QOpenGLExtraFunctions {
     Q_OBJECT
 public:
+    Viewport(QWidget* parent = nullptr);
 
-signals:
+    auto& renderables() const { return _renderables; }
+    void setRenderables(const std::vector<SP<Renderable>> &renderables) { _renderables = renderables; }
 
-public slots:
+    void render(const SP<Operations>& operations);
+
+    glm::dvec2 mapQtToGL(const QPoint& p) const;
+    double widgetPixelRatio() const;
+
+protected:
+    void mousePressEvent(QMouseEvent *event) override;
+    void mouseReleaseEvent(QMouseEvent *event) override;
+    void mouseMoveEvent(QMouseEvent *event) override;
+    void mouseDoubleClickEvent(QMouseEvent *event) override;
+
+private:
+    std::optional<std::pair<SP<Renderable>, HitResult>> hitTest(glm::dvec2 pos, const Camera& camera);
+
+    std::vector<SP<Renderable>> _renderables;
+    std::optional<SP<Renderable>> _draggedRenderable;
+    HitResult _hitResult;
+    Camera _camera;
 };
 
-} // namespace Render
+} // namespace Renderer
 } // namespace Lattice
