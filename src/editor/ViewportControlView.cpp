dbef52f7c79f81b925ba06db9fb5348a607bac77
Ryohei Ikegami <iofg2100@gmail.com>
2019-01-24 00:39:56 +0900

    Add buttons to ViewportControlView

--- src/editor/ViewportControlView.cpp
+++ src/editor/ViewportControlView.cpp
@@ -1,11 +1,31 @@
 #include "ViewportControlView.hpp"
+#include <QRadioButton>
+#include <QVBoxLayout>
 
 namespace Lattice {
 namespace Editor {
 
-ViewportControlView::ViewportControlView(QWidget *parent) : QWidget(parent)
-{
+ViewportControlView::ViewportControlView(QWidget *parent) : QWidget(parent) {
+    auto layout = new QVBoxLayout();
 
+    {
+
+        auto buttonPerspective = new QRadioButton(tr("Perspective"));
+        buttonPerspective->setAutoExclusive(true);
+
+        auto buttonOrthogonal = new QRadioButton(tr("Orthogonal"));
+        buttonOrthogonal->setAutoExclusive(true);
+
+        auto radioButtonsLayout = new QVBoxLayout();
+        radioButtonsLayout->addWidget(buttonPerspective);
+        radioButtonsLayout->addWidget(buttonOrthogonal);
+
+        auto radioButtons = new QWidget();
+        radioButtons->setLayout(radioButtonsLayout);
+        layout->addWidget(radioButtons);
+    }
+
+    setLayout(layout);
 }
 
 } // namespace Editor
