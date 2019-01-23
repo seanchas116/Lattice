f780cb2f9bff1ee7e4a70de95967d950c5df677c
Ryohei Ikegami <iofg2100@gmail.com>
2019-01-24 00:30:13 +0900

    Add ViewportControlView

---
+++ src/editor/ViewportControlView.cpp
@@ -0,0 +1,12 @@
+#include "ViewportControlView.hpp"
+
+namespace Lattice {
+namespace Editor {
+
+ViewportControlView::ViewportControlView(QWidget *parent) : QWidget(parent)
+{
+
+}
+
+} // namespace Editor
+} // namespace Lattice
