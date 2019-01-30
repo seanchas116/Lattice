b70f10e71090ec84da3a967e19119fe6f9254ac9
Ryohei Ikegami <iofg2100@gmail.com>
2019-01-30 23:13:14 +0900

    Better orthographic camera move

--- src/editor/CameraController.cpp
+++ src/editor/CameraController.cpp
@@ -36,7 +36,12 @@ bool CameraController::mouseMove(QMouseEvent *event) {
     switch (_mode) {
     case Mode::Move: {
         glm::dmat2x3 upRight(location.up(), location.right());
-        double ratio = 0.02 / Render::widgetPixelRatio(_widget);
+        double ratio;
+        if (_camera->projection() == Camera::Projection::Perspective) {
+            ratio = 0.02 / Render::widgetPixelRatio(_widget);
+        } else {
+            ratio = 1 / (_camera->orthoScale() * Render::widgetPixelRatio(_widget));
+        }
         location.position += upRight * (glm::dvec2(offset.y(), -offset.x()) * ratio);
         _camera->setLocation(location);
         break;
