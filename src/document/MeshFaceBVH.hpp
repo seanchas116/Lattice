3eb3364069fb20673b67c1315ada89916204b8e0
Ryohei Ikegami <iofg2100@gmail.com>
2019-02-11 00:43:16 +0900

    WIP

--- src/document/MeshFaceBVH.hpp
+++ src/document/MeshFaceBVH.hpp
@@ -7,6 +7,16 @@ namespace Document {
 class MeshFaceBVH {
 public:
     MeshFaceBVH(const SP<Mesh>& mesh);
+
+    std::optional<std::pair<SP<Document::MeshFace>, float>> pick(const Ray<float>& ray) const;
+
+private:
+    struct Node {
+        Box<float> boundingBox;
+        std::array<int, 2> childIndices;
+        std::vector<SP<MeshFace>> faces;
+    };
+    std::vector<Node> nodes;
 };
 
 } // namespace Document
