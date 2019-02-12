a1b7d9133f2e7097a6d2a0154c7f0b756a93b578
Ryohei Ikegami <iofg2100@gmail.com>
2019-02-13 03:16:07 +0900

    WIP add PickableMap

---
+++ src/render/PickableMap.hpp
@@ -0,0 +1,32 @@
+#pragma once
+
+#include "../support/Shorthands.hpp"
+#include <QtGlobal>
+#include <QVector>
+#include <QOpenGLExtraFunctions>
+#include <glm/glm.hpp>
+
+namespace Lattice::GL {
+class Framebuffer;
+}
+
+namespace Lattice::Render {
+
+class Pickable;
+
+class PickableMap final : protected QOpenGLExtraFunctions {
+    Q_DISABLE_COPY(PickableMap)
+public:
+    PickableMap();
+
+    void resize(glm::ivec2 size);
+    SP<Pickable> pick(glm::vec2 screenPos);
+
+private:
+    SP<GL::Framebuffer> _framebuffer;
+    glm::ivec2 _framebufferSize = {0, 0};
+    glm::vec2 _devicePixelRatio;
+    std::vector<SP<Pickable>> _pickables;
+};
+
+}
