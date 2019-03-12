06061f3e5a9528492f0b2fe9ff6fafeb2545db4e
Ryohei Ikegami <iofg2100@gmail.com>
2019-03-12 19:44:25 +0900

    Implement MeshSelection faces and edges

--- src/document/MeshSelection.cpp
+++ src/document/MeshSelection.cpp
@@ -23,11 +23,49 @@ glm::vec3 MeshSelection::medianPosition() const {
 }
 
 std::unordered_set<SP<Mesh::Edge> > MeshSelection::edges() const {
-    return {}; // TODO
+    std::unordered_map<SP<Mesh::Edge>, int> edgeCounts;
+
+    for (auto& v : vertices) {
+        for (auto& e : v->edges()) {
+            if (edgeCounts.find(e->sharedFromThis()) != edgeCounts.end()) {
+                ++edgeCounts[e->sharedFromThis()];
+            } else {
+                edgeCounts[e->sharedFromThis()] = 1;
+            }
+        }
+    }
+    std::unordered_set<SP<Mesh::Edge>> edges;
+
+    for (auto& [edge, count] : edgeCounts) {
+        if (count == 2) {
+            edges.insert(edge);
+        }
+    }
+
+    return edges;
 }
 
 std::unordered_set<SP<Mesh::Face> > MeshSelection::faces() const {
-    return {}; // TODO
+    std::unordered_map<SP<Mesh::Face>, int> faceCounts;
+
+    for (auto& v : vertices) {
+        for (auto& f : v->faces()) {
+            if (faceCounts.find(f->sharedFromThis()) != faceCounts.end()) {
+                ++faceCounts[f->sharedFromThis()];
+            } else {
+                faceCounts[f->sharedFromThis()] = 1;
+            }
+        }
+    }
+    std::unordered_set<SP<Mesh::Face>> faces;
+
+    for (auto& [face, count] : faceCounts) {
+        if (count == face->vertices().size()) {
+            faces.insert(face);
+        }
+    }
+
+    return faces;
 }
 
 } // namespace Document
