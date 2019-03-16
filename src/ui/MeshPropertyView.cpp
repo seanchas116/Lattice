6c00aa8161f067217813e7adffd133eff7274df4
Ryohei Ikegami <iofg2100@gmail.com>
2019-03-16 22:52:54 +0900

    Connect to item

--- src/ui/MeshPropertyView.cpp
+++ src/ui/MeshPropertyView.cpp
@@ -1,4 +1,7 @@
 #include "MeshPropertyView.hpp"
+#include "../document/MeshItem.hpp"
+#include "../document/Document.hpp"
+#include "../state/AppState.hpp"
 
 namespace Lattice {
 namespace UI {
@@ -7,6 +10,25 @@ MeshPropertyView::MeshPropertyView(const SP<State::AppState> &appState, QWidget 
     QWidget(parent),
     _appState(appState)
 {
+    setItem(appState->document()->editedItem());
+    connect(appState->document().get(), &Document::Document::editedItemChanged, this, &MeshPropertyView::setItem);
+}
+
+void MeshPropertyView::setItem(const Opt<SP<Document::MeshItem>> &maybeItem) {
+    disconnect(_connection);
+    _item = maybeItem;
+    if (!maybeItem) {
+        return;
+    }
+    auto item = *maybeItem;
+
+    _connection = connect(item->mesh().get(), &Mesh::Mesh::changed, this, [this] {
+        updateViews();
+    });
+    updateViews();
+}
+
+void MeshPropertyView::updateViews() {
 }
 
 } // namespace UI
--- src/ui/MeshPropertyView.hpp
+++ src/ui/MeshPropertyView.hpp
@@ -8,6 +8,9 @@ namespace Lattice {
 namespace State {
 class AppState;
 }
+namespace Document {
+class MeshItem;
+}
 
 namespace UI {
 
@@ -17,7 +20,12 @@ public:
     explicit MeshPropertyView(const SP<State::AppState> &appState, QWidget *parent = nullptr);
 
 private:
+    void setItem(const Opt<SP<Document::MeshItem>>& item);
+    void updateViews();
+
     SP<State::AppState> _appState;
+    Opt<SP<Document::MeshItem>> _item;
+    QMetaObject::Connection _connection;
 };
 
 } // namespace UI
