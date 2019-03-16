57c5090629e3254ad2b82a33949e9a5c37373833
Ryohei Ikegami <iofg2100@gmail.com>
2019-03-16 21:17:52 +0900

    Add stub MeshPropertyView

---
+++ src/ui/MeshPropertyView.cpp
@@ -0,0 +1,10 @@
+#include "MeshPropertyView.hpp"
+
+namespace Lattice {
+namespace UI {
+
+MeshPropertyView::MeshPropertyView(QWidget *parent) : QWidget(parent) {
+}
+
+} // namespace UI
+} // namespace Lattice
---
+++ src/ui/MeshPropertyView.hpp
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <QWidget>
+
+namespace Lattice {
+namespace UI {
+
+class MeshPropertyView : public QWidget {
+    Q_OBJECT
+public:
+    explicit MeshPropertyView(QWidget *parent = nullptr);
+
+signals:
+
+public slots:
+};
+
+} // namespace UI
+} // namespace Lattice
