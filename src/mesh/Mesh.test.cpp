46f3a7350dafcd99bb459e523bfd3cd918bb97c2
Ryohei Ikegami <iofg2100@gmail.com>
2019-03-21 15:34:19 +0900

    Add example test

--- src/mesh/Mesh.test.cpp
+++ src/mesh/Mesh.test.cpp
@@ -1,1 +1,11 @@
+#include <catch2/catch.hpp>
 #include "Mesh.hpp"
+
+using namespace Lattice;
+
+TEST_CASE("Mesh") {
+    SECTION("test") {
+        auto mesh = makeShared<Mesh::Mesh>();
+        REQUIRE(mesh->vertices().size() == 0);
+    }
+}
