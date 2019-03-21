82060f3d011690b9e2a63a1227c41d7afb525abe
Ryohei Ikegami <iofg2100@gmail.com>
2019-03-21 16:14:00 +0900

    Add addEdge test

--- test/mesh/Mesh.test.cpp
+++ test/mesh/Mesh.test.cpp
@@ -2,14 +2,32 @@
 #include "../../src/mesh/Mesh.hpp"
 
 using namespace Lattice;
+using namespace glm;
 
 TEST_CASE("Mesh") {
     auto mesh = makeShared<Mesh::Mesh>();
     SECTION("addVertex") {
-        glm::vec3 p(1, 2, 3);
-        mesh->addVertex(p);
+        SECTION("adds verted at specified position") {
+            glm::vec3 p(1, 2, 3);
+            mesh->addVertex(p);
 
-        REQUIRE(mesh->vertices().size() == 1);
-        REQUIRE((*mesh->vertices().begin())->position() == p);
+            REQUIRE(mesh->vertices().size() == 1);
+            REQUIRE((*mesh->vertices().begin())->position() == p);
+        }
+    }
+    SECTION("addEdge") {
+        vec3 p1(1, 2, 3);
+        vec3 p2(4, 5, 6);
+
+        auto v1 = mesh->addVertex(p1);
+        auto v2 = mesh->addVertex(p2);
+        auto edge = mesh->addEdge({v1, v2});
+
+        SECTION("adds edge between two vertices") {
+            REQUIRE(mesh->edges().size() == 1);
+            REQUIRE(mesh->edges().begin()->second == edge);
+            REQUIRE(edge->vertices()[0] == v1);
+            REQUIRE(edge->vertices()[1] == v2);
+        }
     }
 }
