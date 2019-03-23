3569ea1ea76338f928fe22e7fb69d60872ec19b3
Ryohei Ikegami <iofg2100@gmail.com>
2019-03-23 20:40:52 +0900

    Make private

--- src/support/PropertyChange.hpp
+++ src/support/PropertyChange.hpp
@@ -8,44 +8,45 @@ template <typename TObject, typename TValue, typename TGetter, typename TSetter>
 class PropertyChange : public Change {
 public:
     PropertyChange(const std::unordered_map<SP<TObject>, TValue>& values, TGetter getter, TSetter setter) :
-        newValues(values), getter(getter), setter(setter)
+        _newValues(values), _getter(getter), _setter(setter)
     {
     }
 
     void apply() override {
         std::vector<SP<TObject>> objects;
-        objects.reserve(newValues.size());
+        objects.reserve(_newValues.size());
 
-        for (auto& [obj, value] : newValues) {
-            oldValues[obj] = getter(obj);
-            setter(obj, value);
+        for (auto& [obj, value] : _newValues) {
+            _oldValues[obj] = _getter(obj);
+            _setter(obj, value);
             objects.push_back(obj);
         }
     }
 
     SP<Change> invert() const override {
-        return makeShared<PropertyChange>(oldValues, getter, setter);
+        return makeShared<PropertyChange>(_oldValues, _getter, _setter);
     }
 
     bool mergeWith(const SP<const Change>& other) override {
         LATTICE_OPTIONAL_GUARD(change, dynamicPointerCast<const PropertyChange>(other), return false;)
 
-        auto oldValuesMerged = change->oldValues;
-        for (auto& [obj, value] : oldValues) {
+        auto oldValuesMerged = change->_oldValues;
+        for (auto& [obj, value] : _oldValues) {
             oldValuesMerged[obj] = value;
         }
-        oldValues = oldValuesMerged;
+        _oldValues = oldValuesMerged;
 
-        for (auto& [obj, value] : change->newValues) {
-            newValues[obj] = value;
+        for (auto& [obj, value] : change->_newValues) {
+            _newValues[obj] = value;
         }
         return true;
     }
 
-    std::unordered_map<SP<TObject>, TValue> oldValues;
-    std::unordered_map<SP<TObject>, TValue> newValues;
-    TGetter getter;
-    TSetter setter;
+private:
+    std::unordered_map<SP<TObject>, TValue> _oldValues;
+    std::unordered_map<SP<TObject>, TValue> _newValues;
+    TGetter _getter;
+    TSetter _setter;
 };
 
 template <typename TObject, typename TValue, typename TGetter, typename TSetter>
