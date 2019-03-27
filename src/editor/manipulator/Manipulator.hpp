48c391a576ddffe7617ddeb38f2c2b54dab48215
Ryohei Ikegami <iofg2100@gmail.com>
2019-03-27 18:06:03 +0900

    Tweak

--- src/editor/manipulator/Manipulator.hpp
+++ src/editor/manipulator/Manipulator.hpp
@@ -9,20 +9,16 @@ namespace Manipulator {
 
 class Manipulator : public QObject {
     Q_OBJECT
+
 public:
+    enum class ValueType {
+        Translate, Scale, Rotate
+    };
+
     Manipulator();
 
     glm::dvec3 targetPosition() const { return _targetPosition; }
     void setTargetPosition(glm::dvec3 position);
-
-    auto& translateHandles() const { return _translateHandles; }
-    auto& rotateHandles() const { return _rotateHandles; }
-    auto& scaleHandles() const { return _scaleHandles; }
-    std::vector<SP<Render::RenderableObject>> handles(bool withTranslate, bool withRotate, bool withScale) const;
-
-    enum class ValueType {
-        Translate, Scale, Rotate
-    };
 
     bool isTranslateHandleVisible() const { return _isTranslateHandleVisible; }
     void setIsTranslateHandleVisible(bool isTranslateHandleVisible);
@@ -33,12 +29,17 @@ public:
     bool isScaleHandleVisible() const { return _isScaleHandleVisible; }
     void setIsScaleHandleVisible(bool isScaleHandleVisible);
 
+    auto& translateHandles() const { return _translateHandles; }
+    auto& rotateHandles() const { return _rotateHandles; }
+    auto& scaleHandles() const { return _scaleHandles; }
+    std::vector<SP<Render::RenderableObject>> handles(bool withTranslate, bool withRotate, bool withScale) const;
+
 signals:
+    void targetPositionChanged(glm::dvec3 position);
+
     void isTranslateHandleVisibleChanged(bool isTranslateHandleVisible);
     void isRotateHandleVisibleChanged(bool isRotateHandleVisible);
     void isScaleHandleVisibleChanged(bool isScaleHandleVisible);
-
-    void targetPositionChanged(glm::dvec3 position);
 
     void onBegin(ValueType type, double value);
     void onChange(ValueType type, int axis, double value);
