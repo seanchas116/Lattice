8b87a039d7578038e7fe2d42fc3b12f39fb526ef
Ryohei Ikegami <iofg2100@gmail.com>
2019-03-28 23:28:58 +0900

    Add DrawCircle::draw2D

--- src/render/operations/DrawCircle.cpp
+++ src/render/operations/DrawCircle.cpp
@@ -3,6 +3,9 @@
 #include "../../resource/Resource.hpp"
 #include "../../support/Location.hpp"
 #include "../../support/Camera.hpp"
+#include <glm/gtx/transform.hpp>
+
+using namespace glm;
 
 namespace Lattice {
 namespace Render {
@@ -20,6 +23,18 @@ void DrawCircle::draw(const SP<GL::VAO> &vao, const glm::dmat4 &matrix, const SP
     _shader.setUniform("color", color);
     _shader.setUniform("useVertexColor", useVertexColor);
     _shader.setUniform("zOffset", zOffset);
+    vao->draw();
+}
+
+void DrawCircle::draw2D(const SP<GL::VAO> &vao, const glm::dmat4 &matrix, glm::ivec2 viewportSize, double width, glm::vec4 color, bool useVertexColor) {
+    _shader.bind();
+    dmat4 MVP = translate(dvec3(-1.0)) * scale(dvec3(2.0 / dvec2(viewportSize), 2.0)) * matrix;
+    _shader.setUniform("MVP", MVP);
+    _shader.setUniform("viewportSize", viewportSize);
+    _shader.setUniform("width", width);
+    _shader.setUniform("color", color);
+    _shader.setUniform("useVertexColor", useVertexColor);
+    _shader.setUniform("zOffset", 0);
     vao->draw();
 }
 
