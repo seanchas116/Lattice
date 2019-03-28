0aa630223549348134e7d806b7280d2464a0ddb0
Ryohei Ikegami <iofg2100@gmail.com>
2019-03-28 23:40:02 +0900

    Add functions

--- src/editor/manipulator/CenterPickable.cpp
+++ src/editor/manipulator/CenterPickable.cpp
@@ -7,6 +7,30 @@ namespace Manipulator {
 CenterPickable::CenterPickable() {
 }
 
+void CenterPickable::draw(const SP<Render::Operations> &operations, const SP<Camera> &camera) {
+    Q_UNUSED(operations); Q_UNUSED(camera);
+}
+
+void CenterPickable::drawPickables(const SP<Render::Operations> &operations, const SP<Camera> &camera) {
+    Q_UNUSED(operations); Q_UNUSED(camera);
+}
+
+void CenterPickable::mousePressEvent(const Render::MouseEvent &event) {
+    Q_UNUSED(event);
+}
+
+void CenterPickable::mouseMoveEvent(const Render::MouseEvent &event) {
+    Q_UNUSED(event);
+}
+
+void CenterPickable::mouseReleaseEvent(const Render::MouseEvent &event) {
+    Q_UNUSED(event);
+}
+
+void CenterPickable::contextMenuEvent(const Render::ContextMenuEvent &event) {
+    emit onContextMenu(event);
+}
+
 } // namespace Manipulator
 } // namespace Editor
 } // namespace Lattice
--- src/editor/manipulator/CenterPickable.hpp
+++ src/editor/manipulator/CenterPickable.hpp
@@ -9,6 +9,21 @@ class CenterPickable : public Render::RenderableObject {
     Q_OBJECT
 public:
     CenterPickable();
+
+    void draw(const SP<Render::Operations> &operations, const SP<Camera> &camera) override;
+    void drawPickables(const SP<Render::Operations> &operations, const SP<Camera> &camera) override;
+    void mousePressEvent(const Render::MouseEvent &event) override;
+    void mouseMoveEvent(const Render::MouseEvent &event) override;
+    void mouseReleaseEvent(const Render::MouseEvent &event) override;
+    void contextMenuEvent(const Render::ContextMenuEvent &event) override;
+
+    void setTargetPosition(glm::dvec3 pos) { _targetPosition = pos; }
+
+signals:
+    void onContextMenu(const Render::ContextMenuEvent& event);
+
+private:
+    glm::dvec3 _targetPosition {0};
 };
 
 } // namespace Manipulator
