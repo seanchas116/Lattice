72b6b1307ca810d985894ec1b7f8ace5501c8cda
Ryohei Ikegami <iofg2100@gmail.com>
2019-03-29 09:56:09 +0900

    WIP render circle

--- src/editor/manipulator/CenterHandle.cpp
+++ src/editor/manipulator/CenterHandle.cpp
@@ -1,4 +1,9 @@
 #include "CenterHandle.hpp"
+#include "../../gl/VAO.hpp"
+#include "../../gl/Vertex.hpp"
+#include "../../gl/VertexBuffer.hpp"
+
+using namespace glm;
 
 namespace Lattice {
 namespace Editor {
@@ -9,6 +14,17 @@ CenterHandle::CenterHandle() {
 
 void CenterHandle::draw(const SP<Render::Operations> &operations, const SP<Camera> &camera) {
     Q_UNUSED(operations); Q_UNUSED(camera);
+    auto [viewportPos, isInViewport] = camera->mapWorldToViewport(_targetPosition);
+    if (!isInViewport) {
+        return;
+    }
+    dvec3 frontPos(viewportPos.xy, 0);
+
+    GL::Vertex vertex { frontPos };
+    auto vbo = makeShared<GL::VertexBuffer<GL::Vertex>>();
+    vbo->setVertices({vertex});
+    auto vao = makeShared<GL::VAO>(vbo, GL::Primitive::Point);
+    operations->drawCircle.draw2D(vao, dmat4(1), camera->viewportSize(), 10, glm::vec4(1, 0, 0, 1));
 }
 
 void CenterHandle::drawPickables(const SP<Render::Operations> &operations, const SP<Camera> &camera) {
