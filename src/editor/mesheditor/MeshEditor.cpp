5bbc4030c7e9b563aada6f89f0ef5f1a4289d15c
Ryohei Ikegami <iofg2100@gmail.com>
2019-04-06 20:34:15 +0900

    Tweak

--- src/editor/mesheditor/MeshEditor.cpp
+++ src/editor/mesheditor/MeshEditor.cpp
@@ -28,7 +28,7 @@ const vec4 hoveredColor = vec4(1, 1, 0, 1);
 
 const vec4 unselectedFaceHighlight = vec4(0, 0, 0, 0);
 const vec4 selectedFaceHighlight = vec4(1, 1, 1, 0.5);
-const vec4 hoveredFaceHighlight = vec4(1, 1, 1, 0.5);
+const vec4 hoveredFaceHighlight = vec4(1, 1, 0.5, 0.5);
 
 }
 
