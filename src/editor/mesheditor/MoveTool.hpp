9f7363cd8ea5c1248387c9a2877e68f1167dd409
Ryohei Ikegami <iofg2100@gmail.com>
2019-04-14 17:19:17 +0900

    Call BorderSelectTool from MoveTool

--- src/editor/mesheditor/MoveTool.hpp
+++ src/editor/mesheditor/MoveTool.hpp
@@ -1,5 +1,6 @@
 #pragma once
 #include "Tool.hpp"
+#include "BorderSelectTool.hpp"
 #include "../../mesh/MeshFragment.hpp"
 
 namespace Lattice {
@@ -8,19 +9,22 @@ namespace MeshEditor {
 
 class MoveTool : public Tool {
 public:
-    MoveTool(const SP<State::AppState>& appState, const SP<Document::MeshObject>& object) : Tool(appState, object) {}
+    MoveTool(const SP<State::AppState>& appState, const SP<Document::MeshObject>& object);
 
     void mousePressEvent(const EventTarget &target, const Viewport::MouseEvent &event) override;
     void mouseMoveEvent(const EventTarget &target, const Viewport::MouseEvent &event) override;
     void mouseReleaseEvent(const EventTarget &target, const Viewport::MouseEvent &event) override;
+    void drawOverlay(QPainter *painter, const QSize &viewportSize) override;
 
 private:
+    BorderSelectTool _borderSelectTool;
     Mesh::MeshFragment _nextSelection;
     std::unordered_map<SP<Mesh::Vertex>, glm::dvec3> _initPositions;
     glm::dvec3 _initObjectPos {0};
     glm::dvec2 _initViewportPos {0};
     bool _dragged {false};
     bool _dragStarted {false};
+    bool _borderSelectMode {false};
 };
 
 } // namespace MeshEditor
