ee652e2145b8b67640cad39a4c5f064216fa2cf1
Ryohei Ikegami <iofg2100@gmail.com>
2019-04-27 20:45:09 +0900

    Fix

--- src/newmesh/Mesh.cpp
+++ src/newmesh/Mesh.cpp
@@ -9,7 +9,7 @@ Mesh::Mesh()
 }
 
 VertexHandle Mesh::addVertex() {
-    uint32_t index = _vertices.size();
+    auto index = uint32_t(_vertices.size());
     _vertices.push_back(Vertex{});
     return VertexHandle(index);
 }
@@ -17,13 +17,13 @@ VertexHandle Mesh::addVertex() {
 UVPointHandle Mesh::addUVPoint(VertexHandle v) {
     UVPoint uvPoint;
     uvPoint.vertex = v.index;
-    uint32_t index = _uvPoints.size();
+    auto index = uint32_t(_uvPoints.size());
     _uvPoints.push_back(uvPoint);
     return UVPointHandle(index);
 }
 
 EdgeHandle Mesh::addEdge(VertexHandle v0, VertexHandle v1) {
-    for (auto&& edge : edges(v0)) {
+    for (auto edge : edges(v0)) {
         auto edgeVertices = vertices(edge);
         if (edgeVertices == std::array{v0, v1} || edgeVertices == std::array{v1, v0}) {
             // edge already exists
@@ -33,7 +33,7 @@ EdgeHandle Mesh::addEdge(VertexHandle v0, VertexHandle v1) {
 
     Edge edge;
     edge.vertices = {v0.index, v1.index};
-    uint32_t index = _edges.size();
+    auto index = uint32_t(_edges.size());
     _edges.push_back(edge);
     return EdgeHandle(index);
 }
