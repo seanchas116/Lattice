9cd8c3119585f513b7c43e6613ea64c579a7cdca
Ryohei Ikegami <iofg2100@gmail.com>
2019-04-27 16:27:52 +0900

    Add classes

--- src/newmesh/Mesh.hpp
+++ src/newmesh/Mesh.hpp
@@ -1,11 +1,52 @@
 #pragma once
+#include <vector>
+#include <array>
+#include <glm/glm.hpp>
 
 namespace Lattice {
 namespace NewMesh {
 
+class Vertex {
+public:
+    bool isDeleted = false;
+    glm::vec3 position;
+    std::vector<int> uvPoints;
+    std::vector<int> edges;
+};
+
+class UVPoint {
+public:
+    glm::vec2 position;
+    int vertex;
+    std::vector<int> faces;
+};
+
+class Edge {
+public:
+    bool isDeleted = false;
+    bool isSmooth = true;
+    std::array<int, 2> vertices;
+    std::vector<int> faces;
+};
+
+class Face {
+public:
+    bool isDeleted = false;
+    std::vector<int> uvPoints;
+    std::vector<int> edges;
+    int material;
+};
+
 class Mesh {
 public:
     Mesh();
+
+    void collectGarbage();
+
+    std::vector<Vertex> vertices;
+    std::vector<UVPoint> uvPoints;
+    std::vector<Edge> edges;
+    std::vector<Face> faces;
 };
 
 } // namespace NewMesh
