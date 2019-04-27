d472ec78b989466afbb0e483b208a6c57e8c3721
Ryohei Ikegami <iofg2100@gmail.com>
2019-04-27 16:22:33 +0900

    Add NewMesh::Mesh

---
+++ src/newmesh/Mesh.cpp
@@ -0,0 +1,12 @@
+#include "Mesh.hpp"
+
+namespace Lattice {
+namespace NewMesh {
+
+Mesh::Mesh()
+{
+
+}
+
+} // namespace NewMesh
+} // namespace Lattice
---
+++ src/newmesh/Mesh.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+namespace Lattice {
+namespace NewMesh {
+
+class Mesh {
+public:
+    Mesh();
+};
+
+} // namespace NewMesh
+} // namespace Lattice
