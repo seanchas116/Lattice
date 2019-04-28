529e06d20bb3cd754112d8f48cfc3a4feb87942a
Ryohei Ikegami <iofg2100@gmail.com>
2019-04-28 12:58:30 +0900

    addFace

--- src/newmesh/Mesh.cpp
+++ src/newmesh/Mesh.cpp
@@ -18,10 +18,11 @@ UVPointHandle Mesh::addUVPoint(VertexHandle v) {
 }
 
 EdgeHandle Mesh::addEdge(VertexHandle v0, VertexHandle v1) {
+    // check if edge already exists
     for (auto edge : edges(v0)) {
         auto edgeVertices = vertices(edge);
         if (edgeVertices == std::array{v0, v1} || edgeVertices == std::array{v1, v0}) {
-            // edge already exists
+            // same edge found
             return edge;
         }
     }
@@ -31,6 +32,36 @@ EdgeHandle Mesh::addEdge(VertexHandle v0, VertexHandle v1) {
     auto index = uint32_t(_edges.size());
     _edges.push_back(edge);
     return EdgeHandle(index);
+}
+
+FaceHandle Mesh::addFace(const std::vector<UVPointHandle> &uvPoints) {
+    // check if face already exists
+    for (auto face : faces(uvPoints[0])) {
+        auto faceUVPoints = this->uvPoints(face) | ranges::to_vector;
+        if (faceUVPoints.size() != uvPoints.size()) {
+            continue;
+        }
+        for (size_t offset = 0; offset < uvPoints.size(); ++offset) {
+            bool isSame = true;
+            for (size_t i = 0; i < uvPoints.size(); ++i) {
+                if (faceUVPoints[(i + offset) % uvPoints.size()] != uvPoints[i]) {
+                    isSame = false;
+                    break;
+                }
+            }
+            if (isSame) {
+                // same face found
+                return face;
+            }
+        }
+    }
+
+    Face face;
+    face.uvPoints = uvPoints | ranges::view::transform([](auto it) { return it.index; }) | ranges::to_vector;
+    // TODO: set edge
+    auto index = uint32_t(_faces.size());
+    _faces.push_back(face);
+    return FaceHandle(index);
 }
 
 } // namespace NewMesh
