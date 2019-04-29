4c10a244e513bac91ed53601e15b81d9866e867f
Ryohei Ikegami <iofg2100@gmail.com>
2019-04-29 18:31:26 +0900

    Undo & redo

--- src/mesh/algorithm/AddCube.cpp
+++ src/mesh/algorithm/AddCube.cpp
@@ -6,27 +6,43 @@ using namespace glm;
 namespace Lattice {
 namespace Mesh {
 
-void AddCube::perform(Mesh &mesh) {
+void AddCube::redo(Mesh &mesh) {
     //   2    3
     // 6    7
     //   0    1
     // 4    5
 
-    auto v0 = mesh.addUVPoint(mesh.addVertex(vec3(minPos.x, minPos.y, minPos.z)), vec2(0));
-    auto v1 = mesh.addUVPoint(mesh.addVertex(vec3(maxPos.x, minPos.y, minPos.z)), vec2(0));
-    auto v2 = mesh.addUVPoint(mesh.addVertex(vec3(minPos.x, maxPos.y, minPos.z)), vec2(0));
-    auto v3 = mesh.addUVPoint(mesh.addVertex(vec3(maxPos.x, maxPos.y, minPos.z)), vec2(0));
-    auto v4 = mesh.addUVPoint(mesh.addVertex(vec3(minPos.x, minPos.y, maxPos.z)), vec2(0));
-    auto v5 = mesh.addUVPoint(mesh.addVertex(vec3(maxPos.x, minPos.y, maxPos.z)), vec2(0));
-    auto v6 = mesh.addUVPoint(mesh.addVertex(vec3(minPos.x, maxPos.y, maxPos.z)), vec2(0));
-    auto v7 = mesh.addUVPoint(mesh.addVertex(vec3(maxPos.x, maxPos.y, maxPos.z)), vec2(0));
+    auto v0 = mesh.addVertex(vec3(minPos.x, minPos.y, minPos.z));
+    auto v1 = mesh.addVertex(vec3(maxPos.x, minPos.y, minPos.z));
+    auto v2 = mesh.addVertex(vec3(minPos.x, maxPos.y, minPos.z));
+    auto v3 = mesh.addVertex(vec3(maxPos.x, maxPos.y, minPos.z));
+    auto v4 = mesh.addVertex(vec3(minPos.x, minPos.y, maxPos.z));
+    auto v5 = mesh.addVertex(vec3(maxPos.x, minPos.y, maxPos.z));
+    auto v6 = mesh.addVertex(vec3(minPos.x, maxPos.y, maxPos.z));
+    auto v7 = mesh.addVertex(vec3(maxPos.x, maxPos.y, maxPos.z));
+    vertices = {v0, v1, v2, v3, v4, v5, v6, v7};
 
-    mesh.addFace({v0, v4, v6, v2}, material);
-    mesh.addFace({v1, v3, v7, v5}, material);
-    mesh.addFace({v0, v1, v5, v4}, material);
-    mesh.addFace({v2, v6, v7, v3}, material);
-    mesh.addFace({v0, v2, v3, v1}, material);
-    mesh.addFace({v4, v5, v7, v6}, material);
+    auto uv0 = mesh.addUVPoint(v0, vec2(0));
+    auto uv1 = mesh.addUVPoint(v1, vec2(0));
+    auto uv2 = mesh.addUVPoint(v2, vec2(0));
+    auto uv3 = mesh.addUVPoint(v3, vec2(0));
+    auto uv4 = mesh.addUVPoint(v4, vec2(0));
+    auto uv5 = mesh.addUVPoint(v5, vec2(0));
+    auto uv6 = mesh.addUVPoint(v6, vec2(0));
+    auto uv7 = mesh.addUVPoint(v7, vec2(0));
+
+    mesh.addFace({uv0, uv4, uv6, uv2}, material);
+    mesh.addFace({uv1, uv3, uv7, uv5}, material);
+    mesh.addFace({uv0, uv1, uv5, uv4}, material);
+    mesh.addFace({uv2, uv6, uv7, uv3}, material);
+    mesh.addFace({uv0, uv2, uv3, uv1}, material);
+    mesh.addFace({uv4, uv5, uv7, uv6}, material);
+}
+
+void AddCube::undo(Mesh &mesh) {
+    for (auto v : vertices) {
+        mesh.removeVertex(v);
+    }
 }
 
 } // namespace Mesh
--- src/mesh/algorithm/AddCube.hpp
+++ src/mesh/algorithm/AddCube.hpp
@@ -8,11 +8,13 @@ class AddCube {
 public:
     AddCube(glm::vec3 minPos, glm::vec3 maxPos, uint32_t material) : minPos(minPos), maxPos(maxPos), material(material) {}
 
-    void perform(Mesh& mesh);
+    void redo(Mesh& mesh);
+    void undo(Mesh& mesh);
 
     glm::vec3 minPos;
     glm::vec3 maxPos;
     uint32_t material;
+    std::vector<VertexHandle> vertices;
 };
 
 } // namespace Mesh
