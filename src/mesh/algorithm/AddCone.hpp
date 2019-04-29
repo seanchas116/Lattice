ddd914dc4f165bf421ac8ebc25f08cf7ac996b89
Ryohei Ikegami <iofg2100@gmail.com>
2019-04-29 20:20:37 +0900

    Add AddCone

---
+++ src/mesh/algorithm/AddCone.hpp
@@ -0,0 +1,30 @@
+#pragma once
+#include "../Mesh.hpp"
+
+namespace Lattice {
+namespace Mesh {
+
+class AddCone {
+public:
+    AddCone(glm::vec3 center, float radius, float height, int segmentCount, int axis, uint32_t material) : center(center),
+                                                                                                           radius(radius),
+                                                                                                           height(height),
+                                                                                                           segmentCount(segmentCount),
+                                                                                                           axis(axis),
+                                                                                                           material(material) {}
+
+    void redo(Mesh& mesh);
+    void undo(Mesh& mesh);
+
+    glm::vec3 center;
+    float radius;
+    float height;
+    int segmentCount;
+    int axis;
+    uint32_t material;
+    std::vector<VertexHandle> vertices;
+};
+
+} // namespace Mesh
+} // namespace Lattice
+
