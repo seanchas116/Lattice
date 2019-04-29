1425de1315e1ed91f2572743769eb8255f84e51d
Ryohei Ikegami <iofg2100@gmail.com>
2019-04-29 17:57:25 +0900

    Implement AddCube

--- src/mesh/algorithm/AddCube.cpp
+++ src/mesh/algorithm/AddCube.cpp
@@ -1,14 +1,32 @@
 #include "AddCube.hpp"
 #include <QtGlobal>
 
+using namespace glm;
+
 namespace Lattice {
 namespace Mesh {
 
 void AddCube::perform(Mesh &mesh) {
-    Q_UNUSED(mesh)
-    Q_UNUSED(_minPos)
-    Q_UNUSED(_maxPos)
-    Q_UNUSED(_material)
+    //   2    3
+    // 6    7
+    //   0    1
+    // 4    5
+
+    auto v0 = mesh.addUVPoint(mesh.addVertex(vec3(minPos.x, minPos.y, minPos.z)), vec2(0));
+    auto v1 = mesh.addUVPoint(mesh.addVertex(vec3(maxPos.x, minPos.y, minPos.z)), vec2(0));
+    auto v2 = mesh.addUVPoint(mesh.addVertex(vec3(minPos.x, maxPos.y, minPos.z)), vec2(0));
+    auto v3 = mesh.addUVPoint(mesh.addVertex(vec3(maxPos.x, maxPos.y, minPos.z)), vec2(0));
+    auto v4 = mesh.addUVPoint(mesh.addVertex(vec3(minPos.x, minPos.y, maxPos.z)), vec2(0));
+    auto v5 = mesh.addUVPoint(mesh.addVertex(vec3(maxPos.x, minPos.y, maxPos.z)), vec2(0));
+    auto v6 = mesh.addUVPoint(mesh.addVertex(vec3(minPos.x, maxPos.y, maxPos.z)), vec2(0));
+    auto v7 = mesh.addUVPoint(mesh.addVertex(vec3(maxPos.x, maxPos.y, maxPos.z)), vec2(0));
+
+    mesh.addFace({v0, v4, v6, v2}, material);
+    mesh.addFace({v1, v3, v7, v5}, material);
+    mesh.addFace({v0, v1, v5, v4}, material);
+    mesh.addFace({v2, v6, v7, v3}, material);
+    mesh.addFace({v0, v2, v3, v1}, material);
+    mesh.addFace({v4, v5, v7, v6}, material);
 }
 
 } // namespace Mesh
--- src/mesh/algorithm/AddCube.hpp
+++ src/mesh/algorithm/AddCube.hpp
@@ -6,14 +6,13 @@ namespace Mesh {
 
 class AddCube {
 public:
-    AddCube(glm::vec3 minPos, glm::vec3 maxPos, uint32_t material) : _minPos(minPos), _maxPos(maxPos), _material(material) {}
+    AddCube(glm::vec3 minPos, glm::vec3 maxPos, uint32_t material) : minPos(minPos), maxPos(maxPos), material(material) {}
 
     void perform(Mesh& mesh);
 
-private:
-    glm::vec3 _minPos;
-    glm::vec3 _maxPos;
-    uint32_t _material;
+    glm::vec3 minPos;
+    glm::vec3 maxPos;
+    uint32_t material;
 };
 
 } // namespace Mesh
