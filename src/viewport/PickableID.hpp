9416d522e9e5a2b415538b92d4104f11763cbcf3
Ryohei Ikegami <iofg2100@gmail.com>
2019-05-01 15:28:32 +0900

    Add fromIDColor

--- src/viewport/PickableID.hpp
+++ src/viewport/PickableID.hpp
@@ -5,6 +5,17 @@ namespace Lattice {
 namespace Viewport {
 
 class PickableID {
+    struct Data {
+        struct {
+            int renderableID;
+            int fragmentID;
+        };
+        glm::u16vec4 color;
+    };
+
+    int _renderableID;
+    int _fragmentID;
+
 public:
     PickableID(int renderableID, int fragmentID = 0) : _renderableID(renderableID), _fragmentID(fragmentID) {}
 
@@ -13,21 +24,17 @@ public:
     }
 
     glm::vec4 toColor() const {
-        union {
-            struct {
-                int renderableID;
-                int fragmentID;
-            };
-            glm::u16vec4 color;
-        } data;
+        Data data;
         data.renderableID = _renderableID;
         data.fragmentID = _fragmentID;
         return data.color;
     }
 
-private:
-    int _renderableID;
-    int _fragmentID;
+    static PickableID fromIDColor(glm::vec4 color) {
+        Data data;
+        data.color = color;
+        return PickableID(data.renderableID, data.fragmentID);
+    }
 };
 
 } // namespace Viewport
