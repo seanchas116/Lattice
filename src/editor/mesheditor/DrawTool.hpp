d046a716a5ba33d3e75d80026c25d06bf4d315d9
Ryohei Ikegami <iofg2100@gmail.com>
2019-05-01 22:05:37 +0900

    WIP add DrawTool

---
+++ src/editor/mesheditor/DrawTool.hpp
@@ -0,0 +1,27 @@
+#pragma once
+#include "Tool.hpp"
+
+namespace Lattice {
+namespace Editor {
+namespace MeshEditor {
+
+class DrawTool : public Tool {
+public:
+    DrawTool(const SP<State::AppState>& appState, const SP<Document::MeshObject>& object, const SP<Mesh::Mesh>& mesh) : Tool(appState, object, mesh) {}
+
+    HitTestExclusion hitTestExclusion() const override;
+
+    void mousePressTool(const EventTarget &target, const Viewport::MouseEvent &event) override;
+    void mouseMoveTool(const EventTarget &target, const Viewport::MouseEvent &event) override;
+    void mouseReleaseTool(const EventTarget &target, const Viewport::MouseEvent &event) override;
+    void keyPressTool(QKeyEvent *event) override;
+
+private:
+    // draw
+    std::vector<Mesh::UVPointHandle> _drawnUVPoints;
+    Opt<Mesh::UVPointHandle> _previewUVPoint;
+};
+
+} // namespace MeshEditor
+} // namespace Editor
+} // namespace Lattice
