2f64f0e089b5bb3cf44e27a250524704f7544687
Ryohei Ikegami <iofg2100@gmail.com>
2019-05-03 01:21:29 +0900

    Refactor

--- src/mesh/Handle.hpp
+++ src/mesh/Handle.hpp
@@ -6,42 +6,33 @@
 namespace Lattice {
 namespace Mesh {
 
-template <typename TDerived>
+template <typename Tag>
 struct Handle {
+    Handle() : index(0) {}
     explicit Handle(uint32_t index) : index(index) {}
-    bool operator==(const TDerived& other) const { return index == other.index; }
-    bool operator!=(const TDerived& other) const { return !operator==(other); }
+    bool operator==(const Handle& other) const { return index == other.index; }
+    bool operator!=(const Handle& other) const { return !operator==(other); }
     uint32_t index;
 };
 
-struct VertexHandle : public Handle<VertexHandle> {
-    VertexHandle() : Handle(0) {}
-    explicit VertexHandle(uint32_t index) : Handle(index) {}
-};
+struct VertexHandleTag {};
+struct UVPointHandleTag {};
+struct EdgeHandleTag {};
+struct FaceHandleTag {};
 
-struct UVPointHandle : public Handle<UVPointHandle> {
-    UVPointHandle() : Handle(0) {}
-    explicit UVPointHandle(uint32_t index) : Handle(index) {}
-};
-
-struct EdgeHandle : public Handle<EdgeHandle> {
-    EdgeHandle() : Handle(0) {}
-    EdgeHandle(uint32_t index) : Handle(index) {}
-};
-
-struct FaceHandle : public Handle<FaceHandle> {
-    FaceHandle() : Handle(0) {}
-    FaceHandle(uint32_t index) : Handle(index) {}
-};
+using VertexHandle = Handle<VertexHandleTag>;
+using UVPointHandle = Handle<UVPointHandleTag>;
+using EdgeHandle = Handle<EdgeHandleTag>;
+using FaceHandle = Handle<FaceHandleTag>;
 
 } // namespace NewMesh
 } // namespace Lattice
 
 namespace std {
 
-template <>
-struct hash<Lattice::Mesh::VertexHandle> {
-    size_t operator()(const Lattice::Mesh::VertexHandle& value) const {
+template <typename Tag>
+struct hash<Lattice::Mesh::Handle<Tag>> {
+    size_t operator()(const Lattice::Mesh::Handle<Tag>& value) const {
         return std::hash<uint32_t>()(value.index);
     }
 };
