c35e277b45ac7af17a1d385ea0caf01bfc02fbb2
Ryohei Ikegami <iofg2100@gmail.com>
2019-05-02 16:37:58 +0900

    Separate Handle.hpp

---
+++ src/mesh/Handle.hpp
@@ -0,0 +1,37 @@
+#pragma once
+
+#include <cstdint>
+
+namespace Lattice {
+namespace Mesh {
+
+template <typename TDerived>
+struct Handle {
+    explicit Handle(uint32_t index) : index(index) {}
+    bool operator==(const TDerived& other) const { return index == other.index; }
+    bool operator!=(const TDerived& other) const { return !operator==(other); }
+    uint32_t index;
+};
+
+struct VertexHandle : public Handle<VertexHandle> {
+    VertexHandle() : Handle(0) {}
+    explicit VertexHandle(uint32_t index) : Handle(index) {}
+};
+
+struct UVPointHandle : public Handle<UVPointHandle> {
+    UVPointHandle() : Handle(0) {}
+    explicit UVPointHandle(uint32_t index) : Handle(index) {}
+};
+
+struct EdgeHandle : public Handle<EdgeHandle> {
+    EdgeHandle() : Handle(0) {}
+    EdgeHandle(uint32_t index) : Handle(index) {}
+};
+
+struct FaceHandle : public Handle<FaceHandle> {
+    FaceHandle() : Handle(0) {}
+    FaceHandle(uint32_t index) : Handle(index) {}
+};
+
+} // namespace NewMesh
+} // namespace Lattice
--- src/mesh/Mesh.hpp
+++ src/mesh/Mesh.hpp
@@ -3,40 +3,10 @@
 #include <array>
 #include <glm/glm.hpp>
 #include <range/v3/all.hpp>
+#include "Handle.hpp"
 
 namespace Lattice {
 namespace Mesh {
-
-class Mesh;
-
-template <typename TDerived>
-struct Handle {
-    explicit Handle(uint32_t index) : index(index) {}
-    bool operator==(const TDerived& other) const { return index == other.index; }
-    bool operator!=(const TDerived& other) const { return !operator==(other); }
-    uint32_t index;
-};
-
-struct VertexHandle : public Handle<VertexHandle> {
-    VertexHandle() : Handle(0) {}
-    explicit VertexHandle(uint32_t index) : Handle(index) {}
-};
-
-struct UVPointHandle : public Handle<UVPointHandle> {
-    UVPointHandle() : Handle(0) {}
-    explicit UVPointHandle(uint32_t index) : Handle(index) {}
-};
-
-struct EdgeHandle : public Handle<EdgeHandle> {
-    EdgeHandle() : Handle(0) {}
-    EdgeHandle(uint32_t index) : Handle(index) {}
-};
-
-struct FaceHandle : public Handle<FaceHandle> {
-    FaceHandle() : Handle(0) {}
-    FaceHandle(uint32_t index) : Handle(index) {}
-};
-
 
 struct VertexData {
     bool isDeleted = false;
