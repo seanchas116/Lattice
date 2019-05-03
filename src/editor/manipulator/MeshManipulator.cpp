6e5282dee8075329027608eca6dc819e6358b2a7
Ryohei Ikegami <iofg2100@gmail.com>
2019-05-03 23:27:59 +0900

    WIP

--- src/editor/manipulator/MeshManipulator.cpp
+++ src/editor/manipulator/MeshManipulator.cpp
@@ -1,14 +1,32 @@
 #include "MeshManipulator.hpp"
-#include "../../state/AppState.hpp"
-#include "../../document/Document.hpp"
-#include "../../document/MeshObject.hpp"
-#include "../../document/History.hpp"
+#include "../../mesh/Mesh.hpp"
 
 using namespace glm;
 
 namespace Lattice {
 namespace Editor {
 namespace Manipulator {
+
+namespace {
+
+template <typename TPositions>
+glm::vec3 medianPosition(const TPositions& positions) {
+    if (positions.empty()) {
+        return vec3(0);
+    }
+
+    glm::vec3 minPos(std::numeric_limits<double>::infinity());
+    glm::vec3 maxPos(-std::numeric_limits<double>::infinity());
+
+    for (auto p : positions) {
+        minPos = min(minPos, p);
+        maxPos = max(maxPos, p);
+    }
+
+    return (minPos + maxPos) * 0.5f;
+}
+
+}
 
 MeshManipulator::MeshManipulator(const glm::dmat4& objectToWorldMatrix, const SP<Mesh::Mesh> &mesh) : _objectToWorld(objectToWorldMatrix),
                                                                                                       _worldToObject(inverse(objectToWorldMatrix)),
@@ -24,10 +42,16 @@ void MeshManipulator::handleOnDragBegin(ValueType type, dvec3 values) {
     Q_UNUSED(type);
     _initialValues = values;
 
-    for (auto& v : _appState->document()->meshSelection().vertices) {
-        _initialPositions[v] = (_objectToWorld * dvec4(v->position(), 1)).xyz;
+    auto& mesh = *_mesh;
+
+    auto selectedVertices = mesh.selectedVertices() | ranges::to_vector;
+    auto medianPos = medianPosition(selectedVertices | ranges::view::transform([&](auto v) { return mesh.position(v); }));
+
+    for (auto v : selectedVertices) {
+        _initialPositions[v] = (_objectToWorld * dvec4(mesh.position(v), 1)).xyz;
     }
-    _initialMedianPos = (_objectToWorld * dvec4(_appState->document()->meshSelection().medianPosition(), 1)).xyz;
+
+    _initialMedianPos = (_objectToWorld * dvec4(medianPos, 1)).xyz;
 }
 
 void MeshManipulator::handleOnDragMove(ValueType type, dvec3 values) {
