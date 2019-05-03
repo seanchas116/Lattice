9cce547eec9534e1fcaf1d270a8827fb9ab16d5b
Ryohei Ikegami <iofg2100@gmail.com>
2019-05-03 23:27:55 +0900

    Use selectedVertices

--- src/editor/mesheditor/ExtrudeTool.cpp
+++ src/editor/mesheditor/ExtrudeTool.cpp
@@ -35,7 +35,7 @@ void ExtrudeTool::mousePressTool(const Tool::EventTarget &target, const Viewport
     if (clickedVertices.empty()) {
         return;
     }
-    _vertices = mesh.vertices() | ranges::view::filter([&] (auto handle) { return mesh.isSelected(handle); }) | ranges::to_<std::unordered_set<Mesh::VertexHandle>>();
+    _vertices = mesh.selectedVertices() | ranges::to_<std::unordered_set<Mesh::VertexHandle>>();
 
     if (!set_includes(_vertices, clickedVertices)) {
         _vertices = clickedVertices;
