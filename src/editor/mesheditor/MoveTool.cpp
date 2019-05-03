c8eec9e8436d125ba11b217da2e4fa1014337694
Ryohei Ikegami <iofg2100@gmail.com>
2019-05-03 13:45:25 +0900

    Add MoveTool

---
+++ src/editor/mesheditor/MoveTool.cpp
@@ -0,0 +1,132 @@
+#include "MoveTool.hpp"
+#include "../../document/Document.hpp"
+#include "../../document/History.hpp"
+#include "../../mesh/Mesh.hpp"
+
+using namespace glm;
+
+namespace Lattice {
+namespace Editor {
+namespace MeshEditor {
+
+namespace {
+
+// TODO: move to support
+template <typename T>
+bool set_includes(const std::unordered_set<T>& set, const std::unordered_set<T>& otherSet) {
+    for (auto& value : otherSet) {
+        if (set.find(value) == set.end()) {
+            return false;
+        }
+    }
+    return true;
+}
+
+}
+
+MoveTool::MoveTool(const SP<State::AppState> &appState, const SP<Document::MeshObject> &object, const SP<Mesh::Mesh> &mesh) : Tool(appState, object, mesh),
+                                                                                                                              _borderSelectTool(makeShared<BorderSelectTool>(appState, object, mesh)) {
+    setChildRenderables({_borderSelectTool});
+    _borderSelectTool->setVisible(false);
+}
+
+void MoveTool::mousePressTool(const Tool::EventTarget &target, const Viewport::MouseEvent &event) {
+    if (event.originalEvent->button() != Qt::LeftButton) {
+        return;
+    }
+
+    auto& mesh = *this->mesh();
+
+    auto clickedVertices = target.vertices();
+    if (clickedVertices.empty()) {
+        _borderSelectMode = true;
+        _borderSelectTool->setVisible(true);
+        _borderSelectTool->mousePressTool(target, event);
+        return;
+    }
+
+    auto oldSelection = mesh.vertices() | ranges::view::filter([&] (auto handle) { return mesh.isSelected(handle); }) | ranges::to_<std::unordered_set<Mesh::VertexHandle>>();
+
+    bool alreadySelected = !clickedVertices.empty() && set_includes(oldSelection, clickedVertices);
+
+    std::unordered_set<Mesh::VertexHandle> selection;
+    if (event.originalEvent->modifiers() & Qt::ShiftModifier) {
+        selection = oldSelection;
+
+        if (alreadySelected) {
+            for (auto& v : clickedVertices) {
+                selection.erase(v);
+            }
+        } else {
+            for (auto& v: clickedVertices) {
+                selection.insert(v);
+            }
+        }
+    } else {
+        selection = clickedVertices;
+    }
+    _nextSelection = selection;
+
+    auto& dragVertices = alreadySelected ? oldSelection : clickedVertices;
+
+    _dragged = true;
+    _initPositions.clear();
+    for (auto& v : dragVertices) {
+        _initPositions[v] = mesh.position(v);
+    }
+    _initObjectPos = (object()->location().matrixToModel() * dvec4(event.worldPos(), 1)).xyz;
+    _initViewportPos = event.viewportPos;
+    _dragStarted = false;
+}
+
+void MoveTool::mouseMoveTool(const Tool::EventTarget &target, const Viewport::MouseEvent &event) {
+    Q_UNUSED(target);
+
+    if (_borderSelectMode) {
+        _borderSelectTool->mouseMoveTool(target, event);
+        return;
+    }
+
+    if (!_dragged) {
+        return;
+    }
+
+    dvec3 objectPos = (object()->location().matrixToModel() * dvec4(event.worldPos(), 1)).xyz;
+    dvec3 offset = objectPos - _initObjectPos;
+
+    if (!_dragStarted) {
+        if (distance(_initViewportPos, dvec2(event.viewportPos.xy)) < appState()->preferences()->moveThreshold()) {
+            return;
+        }
+        appState()->document()->history()->beginChange(tr("Move Vertex"));
+        _dragStarted = true;
+    }
+
+    for (auto& [v, initialPos] : _initPositions) {
+        mesh()->setPosition(v, initialPos + offset);
+    }
+    emit meshChanged();
+}
+
+void MoveTool::mouseReleaseTool(const Tool::EventTarget &target, const Viewport::MouseEvent &event) {
+    if (_borderSelectMode) {
+        _borderSelectTool->mouseReleaseTool(target, event);
+        _borderSelectMode = false;
+        _borderSelectTool->setVisible(false);
+        return;
+    }
+
+    _dragged = false;
+    _initPositions.clear();
+    if (!_dragStarted) {
+        mesh()->clearSelections();
+        for (auto& v : _nextSelection) {
+            mesh()->setSelected(v, true);
+        }
+        emit meshChanged();
+    }
+}
+
+} // namespace MeshEditor
+} // namespace Editor
+} // namespace Lattice
---
+++ src/editor/mesheditor/MoveTool.hpp
@@ -0,0 +1,31 @@
+#pragma once
+#include "Tool.hpp"
+#include "BorderSelectTool.hpp"
+#include "../../oldmesh/MeshFragment.hpp"
+
+namespace Lattice {
+namespace Editor {
+namespace MeshEditor {
+
+class MoveTool : public Tool {
+public:
+    MoveTool(const SP<State::AppState>& appState, const SP<Document::MeshObject>& object, const SP<Mesh::Mesh>& mesh);
+
+    void mousePressTool(const EventTarget &target, const Viewport::MouseEvent &event) override;
+    void mouseMoveTool(const EventTarget &target, const Viewport::MouseEvent &event) override;
+    void mouseReleaseTool(const EventTarget &target, const Viewport::MouseEvent &event) override;
+
+private:
+    SP<BorderSelectTool> _borderSelectTool;
+    std::unordered_set<Mesh::VertexHandle> _nextSelection;
+    std::unordered_map<Mesh::VertexHandle, glm::dvec3> _initPositions;
+    glm::dvec3 _initObjectPos {0};
+    glm::dvec2 _initViewportPos {0};
+    bool _dragged {false};
+    bool _dragStarted {false};
+    bool _borderSelectMode {false};
+};
+
+} // namespace MeshEditor
+} // namespace Editor
+} // namespace Lattice
