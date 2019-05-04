02e90867220c54040ab2398962afa5b4a8904975
Ryohei Ikegami <iofg2100@gmail.com>
2019-05-04 22:25:03 +0900

    Use new mesh in all AppState methods

--- src/state/AppState.cpp
+++ src/state/AppState.cpp
@@ -5,7 +5,12 @@
 #include "../document/MeshObject.hpp"
 #include "../oldmesh/Mesh.hpp"
 #include "../mesh/Mesh.hpp"
+#include "../mesh/algorithm/AddPlane.hpp"
 #include "../mesh/algorithm/AddCube.hpp"
+#include "../mesh/algorithm/AddCircle.hpp"
+#include "../mesh/algorithm/AddSphere.hpp"
+#include "../mesh/algorithm/AddCone.hpp"
+#include "../mesh/algorithm/AddCylinder.hpp"
 #include <QFileDialog>
 #include <QtDebug>
 #include <QFileInfo>
@@ -33,51 +38,79 @@ void AppState::deleteObjects() {
 
 void AppState::addPlane() {
     _document->history()->beginChange(tr("Add Plane"));
+
     auto object = makeShared<Document::MeshObject>();
     object->setName(tr("Plane").toStdString());
-    object->oldMesh()->addPlane(dvec3(0), dvec2(2), 1, object->oldMesh()->addMaterial());
+
+    Mesh::Mesh mesh;
+    Mesh::AddPlane(dvec3(0), dvec2(2), 1, 0).redo(mesh);
+    object->setMesh(std::move(mesh));
+
     _document->insertObjectToCurrentPosition(object);
 }
 
 void AppState::addCube() {
     _document->history()->beginChange(tr("Add Cube"));
+
     auto object = makeShared<Document::MeshObject>();
     object->setName(tr("Cube").toStdString());
+
     Mesh::Mesh mesh;
     Mesh::AddCube(glm::vec3(-1), glm::vec3(1), 0).redo(mesh);
     object->setMesh(std::move(mesh));
+
     _document->insertObjectToCurrentPosition(object);
 }
 
 void AppState::addCircle() {
     _document->history()->beginChange(tr("Add Circle"));
+
     auto object = makeShared<Document::MeshObject>();
     object->setName(tr("Circle").toStdString());
-    object->oldMesh()->addCircle(glm::vec3(0), 1.0, 16, OldMesh::Mesh::CircleFill::Ngon, 1, object->oldMesh()->addMaterial());
+
+    Mesh::Mesh mesh;
+    Mesh::AddCircle(glm::vec3(0), 1.0, 16, 1, 0).redo(mesh);
+    object->setMesh(std::move(mesh));
+
     _document->insertObjectToCurrentPosition(object);
 }
 
 void AppState::addSphere() {
     _document->history()->beginChange(tr("Add Sphere"));
+
     auto object = makeShared<Document::MeshObject>();
     object->setName(tr("Sphere").toStdString());
-    object->oldMesh()->addSphere(glm::vec3(0), 1.0, 16, 8, 1, object->oldMesh()->addMaterial());
+
+    Mesh::Mesh mesh;
+    Mesh::AddSphere(glm::vec3(0), 1.0, 16, 8, 1, 0).redo(mesh);
+    object->setMesh(std::move(mesh));
+
     _document->insertObjectToCurrentPosition(object);
 }
 
 void AppState::addCone() {
     _document->history()->beginChange(tr("Add Cone"));
+
     auto object = makeShared<Document::MeshObject>();
     object->setName(tr("Cone").toStdString());
-    object->oldMesh()->addCone(glm::vec3(0), 1.0, 1.0, 16, 1, object->oldMesh()->addMaterial());
+
+    Mesh::Mesh mesh;
+    Mesh::AddCone(glm::vec3(0), 1.0, 1.0, 16, 1, 0).redo(mesh);
+    object->setMesh(std::move(mesh));
+
     _document->insertObjectToCurrentPosition(object);
 }
 
 void AppState::addCylinder() {
     _document->history()->beginChange(tr("Add Cylinder"));
+
     auto object = makeShared<Document::MeshObject>();
     object->setName(tr("Cylinder").toStdString());
-    object->oldMesh()->addCylinder(glm::vec3(0), 1.0, 1.0, 16, 1, object->oldMesh()->addMaterial());
+
+    Mesh::Mesh mesh;
+    Mesh::AddCylinder(glm::vec3(0), 1.0, 1.0, 16, 1, 0).redo(mesh);
+    object->setMesh(std::move(mesh));
+
     _document->insertObjectToCurrentPosition(object);
 }
 
