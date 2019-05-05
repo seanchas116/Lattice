26ed7a264b8cb1b06550342a48b0506516dac072
Ryohei Ikegami <iofg2100@gmail.com>
2019-05-06 00:24:01 +0900

    Add targetObject

--- src/state/MeshEditState.cpp
+++ src/state/MeshEditState.cpp
@@ -3,5 +3,13 @@
 namespace Lattice {
 namespace State {
 
+void MeshEditState::setTargetObject(Opt<SP<Document::MeshObject>> target) {
+    if (_targetObject == target) {
+        return;
+    }
+    _targetObject = std::move(target);
+    emit targetObjectChanged(target);
+}
+
 } // namespace State
 } // namespace Lattice
--- src/state/MeshEditState.hpp
+++ src/state/MeshEditState.hpp
@@ -1,4 +1,5 @@
 #pragma once
+#include "../document/MeshObject.hpp"
 #include <QObject>
 
 namespace Lattice {
@@ -7,6 +8,14 @@ namespace State {
 class MeshEditState : public QObject {
     Q_OBJECT
 public:
+    auto& targetObject() const { return _targetObject; }
+    void setTargetObject(Opt<SP<Document::MeshObject>> target);
+
+signals:
+    void targetObjectChanged(const Opt<SP<Document::MeshObject>>& target);
+
+private:
+    Opt<SP<Document::MeshObject>> _targetObject;
 };
 
 } // namespace State
