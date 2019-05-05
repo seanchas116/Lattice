a38d4a7c463c89264313996dbbf35520e1377282
Ryohei Ikegami <iofg2100@gmail.com>
2019-05-06 00:40:11 +0900

    Implement more

--- src/state/MeshEditState.cpp
+++ src/state/MeshEditState.cpp
@@ -1,7 +1,14 @@
 #include "MeshEditState.hpp"
+#include "../document/Document.hpp"
+#include "../document/History.hpp"
+#include "../mesh/Mesh.hpp"
 
 namespace Lattice {
 namespace State {
+
+MeshEditState::MeshEditState() :
+    _mesh(makeShared<Mesh::Mesh>()) {
+}
 
 void MeshEditState::setTargetObject(Opt<SP<Document::MeshObject>> target) {
     if (_targetObject == target) {
@@ -9,6 +16,32 @@ void MeshEditState::setTargetObject(Opt<SP<Document::MeshObject>> target) {
     }
     _targetObject = std::move(target);
     emit targetObjectChanged(target);
+}
+
+void MeshEditState::setMesh(Mesh::Mesh mesh) {
+    *_mesh = std::move(mesh);
+    emit meshChanged(*_mesh);
+}
+
+void MeshEditState::notifyMeshChange() {
+    emit meshChanged(*_mesh);
+}
+
+void MeshEditState::commitMeshChange(const QString &changeTitle) {
+    if (!_targetObject) {
+        return;
+    }
+    auto object = *_targetObject;
+
+    auto maybeDocument = object->document();
+    if (!maybeDocument) {
+        return;
+    }
+    auto document = *maybeDocument;
+
+    _mesh->collectGarbage();
+    document->history()->beginChange(changeTitle);
+    object->setMesh(*_mesh);
 }
 
 } // namespace State
--- src/state/MeshEditState.hpp
+++ src/state/MeshEditState.hpp
@@ -3,19 +3,34 @@
 #include <QObject>
 
 namespace Lattice {
+
+namespace Mesh {
+class Mesh;
+}
+
 namespace State {
 
 class MeshEditState : public QObject {
     Q_OBJECT
 public:
+    MeshEditState();
+
     auto& targetObject() const { return _targetObject; }
     void setTargetObject(Opt<SP<Document::MeshObject>> target);
 
+    auto& mesh() const { return _mesh; }
+    void setMesh(Mesh::Mesh mesh);
+
+    void notifyMeshChange();
+    void commitMeshChange(const QString& changeTitle);
+
 signals:
+    void meshChanged(const Mesh::Mesh& mesh);
     void targetObjectChanged(const Opt<SP<Document::MeshObject>>& target);
 
 private:
     Opt<SP<Document::MeshObject>> _targetObject;
+    SP<Mesh::Mesh> _mesh;
 };
 
 } // namespace State
