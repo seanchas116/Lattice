8de5fe74610b046700616c1eec90a47aa419b1b8
Ryohei Ikegami <iofg2100@gmail.com>
2019-05-22 12:41:52 +0900

    WIP Add SplitSharpEdges algorithm

---
+++ src/mesh/algorithm/SplitSharpEdges.cpp
@@ -0,0 +1,18 @@
+#include "SplitSharpEdges.hpp"
+
+namespace Lattice {
+namespace Mesh {
+
+void SplitSharpEdges::perform(Mesh &mesh) const {
+    for (auto v : mesh.vertices()) {
+        int nSharpEdges = 0;
+        for (auto e : mesh.edges(v)) {
+            if (!mesh.isSmooth(e) || ranges::distance(mesh.faces(e)) >= 3) {
+                ++nSharpEdges;
+            }
+        }
+    }
+}
+
+} // namespace Mesh
+} // namespace Lattice
---
+++ src/mesh/algorithm/SplitSharpEdges.hpp
@@ -0,0 +1,12 @@
+#pragma once
+#include "../Mesh.hpp"
+
+namespace Lattice {
+namespace Mesh {
+
+class SplitSharpEdges {
+    void perform(Mesh& mesh) const;
+};
+
+} // namespace Mesh
+} // namespace Lattice
