17c29fb83c93adea324beb2df51378848403d9b6
Ryohei Ikegami <iofg2100@gmail.com>
2019-05-22 13:03:27 +0900

    WIP

--- src/mesh/algorithm/SplitSharpEdges.cpp
+++ src/mesh/algorithm/SplitSharpEdges.cpp
@@ -5,14 +5,69 @@ namespace Mesh {
 
 void SplitSharpEdges::perform(Mesh &mesh) const {
     for (auto v : mesh.vertices()) {
+        auto faces = mesh.faces(v) | ranges::to_vector;
+        if (faces.empty()) {
+            continue;
+        }
+        auto edges = mesh.edges(v) | ranges::to_vector;
+
         int nSharpEdges = 0;
-        for (auto e : mesh.edges(v)) {
+        for (auto e : edges) {
             if (!mesh.isSmooth(e) || ranges::distance(mesh.faces(e)) >= 3) {
                 ++nSharpEdges;
             }
         }
         if (nSharpEdges <= 1) {
             continue;
+        }
+
+        // 1. split faces into groups by sharp edges
+        // 2. create separate vertices for each group
+
+        std::vector<std::array<FaceHandle, 2>> faceConnections;
+        for (auto e : edges) {
+            if (!mesh.isSmooth(e)) {
+                continue;
+            }
+            auto edgeFaces = mesh.faces(e) | ranges::to_vector;
+            if (edgeFaces.size() != 2) {
+                continue;
+            }
+            faceConnections.push_back({edgeFaces[0], edgeFaces[1]});
+        }
+
+        auto isConnected = [&](FaceHandle f0, FaceHandle f1) {
+            for (auto connection : faceConnections) {
+                if (connection[0] == f0 && connection[1] == f1) {
+                    return true;
+                }
+                if (connection[0] == f1 && connection[1] == f0) {
+                    return true;
+                }
+            }
+            return false;
+        };
+
+        std::vector<std::vector<FaceHandle>> faceGroups;
+        for (auto f : faces) {
+            bool addedToGroup = false;
+            for (auto& faceGroup : faceGroups) {
+                bool connectedToGroup = false;
+                for (auto existingFace : faceGroup) {
+                    if (isConnected(f, existingFace)) {
+                        connectedToGroup = true;
+                        break;
+                    }
+                }
+                if (connectedToGroup) {
+                    faceGroup.push_back(f);
+                    addedToGroup = true;
+                    break;
+                }
+            }
+            if (!addedToGroup) {
+                faceGroups.push_back({f});
+            }
         }
     }
 }
