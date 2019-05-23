1f319a5c04abc2fe2e5ac065b7697f20438c4b3e
Ryohei Ikegami <iofg2100@gmail.com>
2019-05-23 21:59:30 +0900

    WIP

--- src/mesh/algorithm/LoopSelect.cpp
+++ src/mesh/algorithm/LoopSelect.cpp
@@ -4,7 +4,20 @@ namespace Lattice {
 namespace Mesh {
 
 void LoopSelect::perform(Mesh &mesh) const {
-    // TODO
+    std::vector<EdgeHandle> edges;
+
+    auto edge = this->edge;
+    auto lastVertex = mesh.vertices(edge)[0];
+    edges.push_back(edge);
+
+    while (true) {
+        auto vertex = mesh.vertices(edge)[0] == lastVertex ? mesh.vertices(edge)[1] : mesh.vertices(edge)[0];
+        auto vertexFaces = mesh.faces(vertex) | ranges::to_vector;
+        if (vertexFaces.size() != 4) {
+            // extraordinally vertex
+            return;
+        }
+    }
 }
 
 } // namespace Mesh
