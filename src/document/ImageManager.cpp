f101c8d9a6c8c6a0e16887305737271e2b3cf885
Ryohei Ikegami <iofg2100@gmail.com>
2019-05-27 20:28:57 +0900

    WIP

--- src/document/ImageManager.cpp
+++ src/document/ImageManager.cpp
@@ -1,11 +1,21 @@
 #include "ImageManager.hpp"
+#include "Image.hpp"
 
 namespace Lattice {
 namespace Document {
 
-ImageManager::ImageManager()
-{
+ImageManager::ImageManager() {
+}
 
+SP<Image> ImageManager::openImage(const std::string &filePath) {
+    // TODO: check if same image exists
+
+    auto image = makeShared<Image>(filePath);
+    _images.push_back(image);
+
+    emit imagesChanged(_images);
+
+    return image;
 }
 
 } // namespace Document
--- src/document/ImageManager.hpp
+++ src/document/ImageManager.hpp
@@ -1,11 +1,26 @@
 #pragma once
+#include <QObject>
+#include "../support/Shorthands.hpp"
 
 namespace Lattice {
 namespace Document {
 
-class ImageManager {
+class Image;
+
+class ImageManager : public QObject {
+    Q_OBJECT
 public:
     ImageManager();
+
+    auto& images() const { return _images; }
+
+    SP<Image> openImage(const std::string& filePath);
+
+signals:
+    void imagesChanged(const std::vector<SP<Image>>& images);
+
+private:
+    std::vector<SP<Image>> _images;
 };
 
 } // namespace Document
