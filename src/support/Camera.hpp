bc9bad0c3aaa259e33640939d33917c8bd29ceb7
Ryohei Ikegami <iofg2100@gmail.com>
2019-06-09 21:15:51 +0900

    Fix

--- src/support/Camera.hpp
+++ src/support/Camera.hpp
@@ -10,12 +10,12 @@ public:
         Orthographic,
     };
 
-    static Camera perspective(glm::mat4 viewportToCameraMatrix, glm::vec2 viewportSize, double fieldOfView, double zNear, double zFar);
-    static Camera orthographic(glm::mat4 viewportToCameraMatrix, glm::vec2 viewportSize, double scale);
+    static Camera perspective(glm::mat4 viewportToWorldMatrix, glm::vec2 viewportSize, double fieldOfView, double zNear, double zFar);
+    static Camera orthographic(glm::mat4 viewportToWorldMatrix, glm::vec2 viewportSize, double scale);
 
 private:
     Projection _projection;
-    glm::mat4 viewportToCameraMatrix;
+    glm::mat4 _viewportToWorldMatrix;
     glm::vec2 _viewportSize;
     double _fieldOfView;
     double _zNear;
