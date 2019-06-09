d327795d5bed7b844d04cb27c7a6396b1787ca4a
Ryohei Ikegami <iofg2100@gmail.com>
2019-06-09 21:27:04 +0900

    Add methods

--- src/support/Camera.hpp
+++ src/support/Camera.hpp
@@ -1,5 +1,7 @@
 #pragma once
+#include "Ray.hpp"
 #include <glm/glm.hpp>
+#include <tuple>
 
 namespace Lattice {
 
@@ -10,23 +12,40 @@ public:
         Orthographic,
     };
 
-    static Camera perspective(glm::mat4 viewportToWorldMatrix, glm::vec2 viewportSize, float fieldOfView, float zNear, float zFar);
-    static Camera orthographic(glm::mat4 viewportToWorldMatrix, glm::vec2 viewportSize, float scale);
+    static Camera perspective(glm::dmat4 cameraToWorldMatrix, glm::dvec2 viewportSize, double fieldOfView, double zNear, double zFar);
+    static Camera orthographic(glm::dmat4 cameraToWorldMatrix, glm::dvec2 viewportSize, double scale);
+
+    std::pair<glm::dvec3, bool> mapModelToViewport(const glm::dmat4& modelMatrix, glm::dvec3 worldPos) const;
+    glm::dvec3 mapViewportToModel(const glm::dmat4& modelMatrix, glm::dvec3 viewportPosWithDepth) const;
+
+    std::pair<glm::dvec3, bool> mapWorldToViewport(glm::dvec3 worldPos) const;
+    glm::dvec3 mapViewportToWorld(glm::dvec3 viewportPosWithDepth) const;
+
+    // viewport coordinate range:
+    //  0 <= x <= logical window width
+    //  0 <= y <= logical window height
+    //  0 <= z <= 1 (1 is farthest from camera)
+    std::pair<glm::dvec3, bool> mapCameraToViewport(glm::dvec3 cameraPos) const;
+    glm::dvec3 mapViewportToCamera(glm::dvec3 viewportPosWithDepth) const;
+
+    Ray<double> cameraMouseRay(glm::dvec2 viewportPos) const;
+    Ray<double> worldMouseRay(glm::dvec2 viewportPos) const;
+    Ray<double> modelMouseRay(const glm::dmat4& modelMatrix, glm::dvec2 viewportPos) const;
 
 private:
     Camera() {}
 
     Projection _projection;
-    glm::mat4 _viewportToWorldMatrix;
-    glm::vec2 _viewportSize;
-    float _fieldOfView;
-    float _zNear;
-    float _zFar;
-    float _orthoScale;
+    glm::dmat4 _cameraToWorldMatrix;
+    glm::dvec2 _viewportSize;
+    double _fieldOfView;
+    double _zNear;
+    double _zFar;
+    double _orthoScale;
 
-    glm::mat4 _cameraToViewportMatrix;
-    glm::mat4 _worldToCameraMatrix;
-    glm::mat4 _worldToViewportMatrix;
+    glm::dmat4 _cameraToViewportMatrix;
+    glm::dmat4 _worldToCameraMatrix;
+    glm::dmat4 _worldToViewportMatrix;
 };
 
 } // namespace Lattice
