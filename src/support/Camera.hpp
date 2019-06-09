f0ee1d4ad7705e9d8bfb9907328bd476e28bd9ba
Ryohei Ikegami <iofg2100@gmail.com>
2019-06-09 20:53:53 +0900

    Add fields

--- src/support/Camera.hpp
+++ src/support/Camera.hpp
@@ -10,8 +10,21 @@ public:
         Orthographic,
     };
 
-    static Camera perspective(glm::mat4 location, glm::vec2 viewportSize, double fieldOfView, double zNear, double zFar);
-    static Camera orthographic(glm::mat4 location, glm::vec2 viewportSize, double scale);
+    static Camera perspective(glm::mat4 viewportToCameraMatrix, glm::vec2 viewportSize, double fieldOfView, double zNear, double zFar);
+    static Camera orthographic(glm::mat4 viewportToCameraMatrix, glm::vec2 viewportSize, double scale);
+
+private:
+    Projection _projection;
+    glm::mat4 viewportToCameraMatrix;
+    glm::vec2 _viewportSize;
+    double _fieldOfView;
+    double _zNear;
+    double _zFar;
+    double _orthoScale;
+
+    glm::mat4 _cameraToViewportMatrix;
+    glm::mat4 _worldToCameraMatrix;
+    glm::mat4 _worldToViewportMatrix;
 };
 
 } // namespace Lattice
