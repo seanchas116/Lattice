54aecf03e82e180486735c58073770d809e33b98
Ryohei Ikegami <iofg2100@gmail.com>
2019-06-09 20:28:55 +0900

    Add orientation property

--- src/editor/CameraState.hpp
+++ src/editor/CameraState.hpp
@@ -9,15 +9,6 @@ namespace Editor {
 class CameraState : public QObject {
     Q_OBJECT
 
-    LATTICE_AUTO_PROPERTY(Camera::Projection, projection, setProjection, Camera::Projection::Perspective)
-    LATTICE_AUTO_PROPERTY(glm::dvec3, position, setPosition, glm::dvec3(0))
-    LATTICE_AUTO_PROPERTY(glm::dvec3, eulerAngles, setEulerAngles, glm::dvec3(0))
-    LATTICE_AUTO_PROPERTY(glm::dvec2, viewportSize, setViewportSize, glm::dvec2(100, 100))
-    LATTICE_AUTO_PROPERTY(double, fieldOfView, setFieldOfView, glm::radians(60.0))
-    LATTICE_AUTO_PROPERTY(double, zNear, setZNear, 0.1)
-    LATTICE_AUTO_PROPERTY(double, zFar, setZFar, 100.0)
-    LATTICE_AUTO_PROPERTY(double, orthoScale, setOrthoScale, 100.0)
-
 public:
     enum class Orientation {
         None,
@@ -30,6 +21,17 @@ public:
     };
 
     CameraState();
+
+private:
+    LATTICE_AUTO_PROPERTY(Orientation, orientation, setOrientation, Orientation::None)
+    LATTICE_AUTO_PROPERTY(Camera::Projection, projection, setProjection, Camera::Projection::Perspective)
+    LATTICE_AUTO_PROPERTY(glm::dvec3, position, setPosition, glm::dvec3(0))
+    LATTICE_AUTO_PROPERTY(glm::dvec3, eulerAngles, setEulerAngles, glm::dvec3(0))
+    LATTICE_AUTO_PROPERTY(glm::dvec2, viewportSize, setViewportSize, glm::dvec2(100, 100))
+    LATTICE_AUTO_PROPERTY(double, fieldOfView, setFieldOfView, glm::radians(60.0))
+    LATTICE_AUTO_PROPERTY(double, zNear, setZNear, 0.1)
+    LATTICE_AUTO_PROPERTY(double, zFar, setZFar, 100.0)
+    LATTICE_AUTO_PROPERTY(double, orthoScale, setOrthoScale, 100.0)
 };
 
 } // namespace Editor
