44a60fda958f5c407c696f4aa300eeeb779e294e
Ryohei Ikegami <iofg2100@gmail.com>
2019-06-09 21:22:25 +0900

    Implement constructor

--- src/support/Camera.cpp
+++ src/support/Camera.cpp
@@ -1,5 +1,43 @@
 #include "Camera.hpp"
+#include <glm/gtc/matrix_transform.hpp>
 
 namespace Lattice {
+
+Camera Camera::perspective(glm::mat4 viewportToWorldMatrix, glm::vec2 viewportSize, float fieldOfView, float zNear, float zFar) {
+    Camera camera;
+    camera._projection = Projection::Perspective;
+    camera._viewportToWorldMatrix = viewportToWorldMatrix;
+    camera._viewportSize = viewportSize;
+    camera._fieldOfView = fieldOfView;
+    camera._zNear = zNear;
+    camera._zFar = zFar;
+
+    camera._cameraToViewportMatrix = glm::perspective(fieldOfView, viewportSize.x / viewportSize.y, zNear, zFar);
+    camera._worldToCameraMatrix = inverse(viewportToWorldMatrix);
+    camera._worldToViewportMatrix = camera._cameraToViewportMatrix * camera._worldToCameraMatrix;
+
+    return camera;
+}
+
+Camera Camera::orthographic(glm::mat4 viewportToWorldMatrix, glm::vec2 viewportSize, float scale) {
+    float zNear = -10000;
+    float zFar = 10000;
+
+    Camera camera;
+    camera._projection = Projection::Orthographic;
+    camera._viewportToWorldMatrix = viewportToWorldMatrix;
+    camera._viewportSize = viewportSize;
+    camera._fieldOfView = 0;
+    camera._zNear = zNear;
+    camera._zFar = zFar;
+    camera._orthoScale = scale;
+
+    glm::vec2 topRight = viewportSize / scale * 0.5f;
+    camera._cameraToViewportMatrix = glm::ortho(-topRight.x, topRight.x, -topRight.y, topRight.y, zNear, zFar);
+    camera._worldToCameraMatrix = inverse(viewportToWorldMatrix);
+    camera._worldToViewportMatrix = camera._cameraToViewportMatrix * camera._worldToCameraMatrix;
+
+    return camera;
+}
 
 } // namespace Lattice
--- src/support/Camera.hpp
+++ src/support/Camera.hpp
@@ -10,17 +10,19 @@ public:
         Orthographic,
     };
 
-    static Camera perspective(glm::mat4 viewportToWorldMatrix, glm::vec2 viewportSize, double fieldOfView, double zNear, double zFar);
-    static Camera orthographic(glm::mat4 viewportToWorldMatrix, glm::vec2 viewportSize, double scale);
+    static Camera perspective(glm::mat4 viewportToWorldMatrix, glm::vec2 viewportSize, float fieldOfView, float zNear, float zFar);
+    static Camera orthographic(glm::mat4 viewportToWorldMatrix, glm::vec2 viewportSize, float scale);
 
 private:
+    Camera() {}
+
     Projection _projection;
     glm::mat4 _viewportToWorldMatrix;
     glm::vec2 _viewportSize;
-    double _fieldOfView;
-    double _zNear;
-    double _zFar;
-    double _orthoScale;
+    float _fieldOfView;
+    float _zNear;
+    float _zFar;
+    float _orthoScale;
 
     glm::mat4 _cameraToViewportMatrix;
     glm::mat4 _worldToCameraMatrix;
