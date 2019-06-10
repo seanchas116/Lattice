c880cc415c5cf08c7330c720ead981fb623ab8f2
Ryohei Ikegami <iofg2100@gmail.com>
2019-06-11 00:07:22 +0900

    Fix

--- src/support/SetUtil.hpp
+++ src/support/SetUtil.hpp
@@ -5,7 +5,7 @@ namespace Lattice {
 
 template <typename T>
 bool includes(const std::unordered_set<T>& set, const std::unordered_set<T>& otherSet) {
-    for (auto& value : otherSet) {
+    for (auto&& value : otherSet) {
         if (set.find(value) == set.end()) {
             return false;
         }
