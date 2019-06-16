004592e6add4cfe02c347984158ebb8296419d12
Ryohei Ikegami <iofg2100@gmail.com>
2019-06-16 23:45:25 +0900

    Implement flipNormal

--- src/mesh/algorithm/FlipNormal.cpp
+++ src/mesh/algorithm/FlipNormal.cpp
@@ -1,9 +1,14 @@
 #include "FlipNormal.hpp"
+#include <range/v3/view/reverse.hpp>
 
 namespace Lattice {
 namespace Mesh {
 
 FaceHandle flipNormal(Mesh &mesh, FaceHandle face) {
+    auto reverseUVPoints = mesh.uvPoints(face) | ranges::view::reverse | ranges::to_vector;
+    auto newFace = mesh.addFace(reverseUVPoints, mesh.material(face));
+    mesh.removeFace(face);
+    return newFace;
 }
 
 } // namespace Mesh
