f1d1f519ff11019d53bfe1b37f545693d87f3ec5
Ryohei Ikegami <iofg2100@gmail.com>
2019-06-16 18:12:52 +0900

    Add Extrude

---
+++ src/mesh/algorithm/Extrude.cpp
@@ -0,0 +1,11 @@
+#include "Extrude.hpp"
+
+namespace Lattice {
+namespace Mesh {
+
+std::vector<VertexHandle> extrude(Mesh &mesh, const std::vector<VertexHandle> &vertices) {
+    return {}; // TODO
+}
+
+} // namespace Mesh
+} // namespace Lattice
